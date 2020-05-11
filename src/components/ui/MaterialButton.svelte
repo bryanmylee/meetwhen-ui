5d52f0f69767d16f1d12805b3b1b84c9ca086f85
Bryan Lee <bryanmylee@gmail.com>
2020-05-12 05:15:36 +0800

    Implement custom button

---
+++ src/components/ui/MaterialButton.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let label;
+</script>
+
+<button on:click>{label}</button>
+
+<style>
+  button {
+    border: 0;
+    color: white;
+    background-color: var(--primary-1);
+    transition: 0.2s ease opacity, 0.2s ease background-color;
+    padding: 1em;
+    border-radius: 5px;
+  }
+
+  button:focus, button:hover {
+    background-color: var(--primary-1-1);
+    outline: none;
+    box-shadow: var(--box-shadow);
+  }
+
+  button:active {
+    background-color: var(--text-3);
+    transform: scale(0.98);
+    transition: none;
+  }
+</style>
