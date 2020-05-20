c65a2daaab4c1efd24526ad37080de7da3958bf6
Bryan Lee <bryanmylee@gmail.com>
2020-05-20 14:50:18 +0800

    Split column overlay and selection

--- src/components/calendar/joinEvent/JoinEventCalendarOtherUserColumnOverlay.svelte
+++ src/components/calendar/joinEvent/JoinEventCalendarOtherUserColumnOverlay.svelte
@@ -1,39 +1,16 @@
 <script>
-  import { getTop, getHeight } from '../../../utils/selections.js';
+  import JoinEventCalendarOtherUserSelection
+      from './JoinEventCalendarOtherUserSelection.svelte';
   /*
    * { start: Dayjs, end: Dayjs, usernames: string[] }[]
    */
   export let mergedIntervals = [];
   export let maxUsernames = 0;
-
-  let isCollapsed = false;
-  setInterval(() => isCollapsed = true, 4000);
-
-  function getOpacity(usernames) {
-    return usernames.length / maxUsernames;
-  }
 </script>
 
 {#each mergedIntervals as interval}
-  <div
-    class={isCollapsed ? "collapsed" : ""}
-    style="top:{getTop(interval.start)};
-           height:{getHeight(interval.end - interval.start)};
-           opacity:{getOpacity(interval.usernames)}"
-  ></div>
+  <JoinEventCalendarOtherUserSelection
+    {interval}
+    {maxUsernames}
+  />
 {/each}
-
-<style>
-  div {
-    position: absolute;
-    width: var(--col-width);
-    border-radius: 5px;
-    background-color: var(--primary-0);
-    transition: 1s cubic-bezier(0.3, 0.03, 0.1, 0.97) width;
-    pointer-events: all;
-  }
-
-  .collapsed {
-    width: calc(var(--col-width) / 10);
-  }
-</style>
---
+++ src/components/calendar/joinEvent/JoinEventCalendarOtherUserSelection.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { getTop, getHeight } from '../../../utils/selections.js';
+  /*
+   * { start: Dayjs, end: Dayjs, usernames: string[] }[]
+   */
+  export let interval = null;
+  export let maxUsernames = 0;
+
+  let isCollapsed = false;
+  setInterval(() => isCollapsed = true, 1000);
+
+  function getOpacity(usernames) {
+    return usernames.length / maxUsernames;
+  }
+</script>
+
+<div
+  class={isCollapsed ? "collapsed" : ""}
+  style="top:{getTop(interval.start)};
+          height:{getHeight(interval.end - interval.start)};
+          opacity:{getOpacity(interval.usernames)}"
+></div>
+
+<style>
+  div {
+    position: absolute;
+    width: var(--col-width);
+    border-radius: 5px;
+    background-color: var(--primary-0);
+    transition: 1s cubic-bezier(0.3, 0.03, 0.1, 0.97) width;
+    pointer-events: all;
+  }
+
+  .collapsed {
+    width: calc(var(--col-width) / 10);
+  }
+</style>
