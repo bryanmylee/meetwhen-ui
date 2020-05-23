cfd2013f44912add77ea57e3ef7710d08c5cc0d3
Bryan Lee <bryanmylee@gmail.com>
2020-05-23 20:31:07 +0800

    Add user count to popover

--- src/components/calendar/joinEvent/JoinEventCalendarOtherUsersPopover.svelte
+++ src/components/calendar/joinEvent/JoinEventCalendarOtherUsersPopover.svelte
@@ -4,11 +4,19 @@
   export let interval;
   const { start, end } = interval;
   const timeString = `${start.format('HH:mm')} - ${end.format('HH:mm')}`;
+
+  let countString = '';
+  $: {
+    const { length } = interval.usernames;
+    if (length === 1) countString = '1 person:';
+    else countString = `${length} people:`;
+  }
 </script>
 
 <div
   transition:fade="{{ duration: 200 }}">
   <h5>{timeString}</h5>
+  <h5>{countString}</h5>
   {#each interval.usernames as username}
     <p>{username}</p>
   {/each}
@@ -20,7 +28,7 @@
     height: -webkit-max-content;
     background-color: white;
     z-index: 1000;
-    padding: 1em;
+    padding: 0.5em;
     border-radius: 5px;
     box-shadow: var(--box-shadow);
   }
