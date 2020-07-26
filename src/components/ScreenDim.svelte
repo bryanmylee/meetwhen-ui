c25ffec427e656ed139e6aa795fe178f3fef71ad
Bryan Lee <bryanmylee@gmail.com>
2020-07-27 04:26:25 +0800

    Adjust ScreenDim opacity

--- src/components/ScreenDim.svelte
+++ src/components/ScreenDim.svelte
@@ -1,11 +1,14 @@
 <script>
   import { fade } from 'svelte/transition';
+
+  export let opacity = 0.5;
 </script>
 
 <div
   on:click
   class="shade"
   transition:fade={{ duration: 200 }}
+  style={`background-color: rgba(0, 0, 0, ${opacity})`}
 />
 
 <style>
@@ -16,7 +19,6 @@
     z-index: 90;
     width: 100vw;
     height: 100vh;
-    background-color: rgba(0, 0, 0, 0.2);
     display: flex;
     justify-content: center;
     align-items: center;
