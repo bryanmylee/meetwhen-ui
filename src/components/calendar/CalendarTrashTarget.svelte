23b159c224a7a02ed689cb1ef5a4bddf082c2642
Bryan Lee <bryanmylee@gmail.com>
2020-07-28 03:26:50 +0800

    Style trash icon

--- src/components/calendar/CalendarTrashTarget.svelte
+++ src/components/calendar/CalendarTrashTarget.svelte
@@ -1,12 +1,27 @@
-<span class="calendar--trash-target">Trash</span>
+<script>
+  import TrashIcon from './TrashIcon.svelte';
+</script>
+
+<div class="calendar--trash-target">
+  <TrashIcon />
+</div>
 
 <style>
-  span {
+  div {
     position: sticky;
     left: 50%;
     bottom: 1rem;
-    z-index: 13;
-    padding: 1em;
-    background-color: var(--grey-300);
+    z-index: 15;
+    width: 3rem;
+    height: 3rem;
+    border-radius: 500px;
+    padding: 0.8em;
+    background-color: var(--error-50);
+    box-shadow: var(--shadow-med);
+  }
+
+  div:hover {
+    background-color: white;
+    box-shadow: var(--shadow-large);
   }
 </style>
