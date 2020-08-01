107aa2e13a62d48f4aec10d2459d79f96017d8e9
Bryan Lee <bryanmylee@gmail.com>
2020-08-01 21:33:58 +0800

    Fix auto scroll issue
    Due to layout height being updated after JS is loaded, scrollTo seems to
    break.
    
    Therefore, delay the scrollTo action.

--- src/routes/[eventUrl]/_components/calendar/actions/autoScroll.js
+++ src/routes/[eventUrl]/_components/calendar/actions/autoScroll.js
@@ -1,5 +1,9 @@
+import { FRAME_DURATION } from 'src/utils/nextFrame';
+
 export function autoScrollSelf(node, { hour } = {}) {
   const rowHeight = node.offsetHeight / 24;
   const BUFFER = 50;
-  node.parentNode.scrollTo(0, hour * rowHeight - BUFFER);
+  setTimeout(() => {
+    node.parentNode.scrollTo(0, hour * rowHeight - BUFFER);
+  }, FRAME_DURATION);
 }
