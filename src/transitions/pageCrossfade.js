e110f498802fdb840dec8d0d90b1fee2c9c214bf
Bryan Lee <bryanmylee@gmail.com>
2020-08-05 12:25:36 +0800

    Fix page crossfade positioning

--- src/transitions/pageCrossfade.js
+++ src/transitions/pageCrossfade.js
@@ -2,20 +2,22 @@ import { sineOut } from 'svelte/easing';
 
 const duration = 150;
 
-export function fadeIn() {
+export function fadeIn(node) {
   return {
     duration,
     delay: duration,
     easing: sineOut,
     css: (t) => `opacity: ${t}`,
+    tick: () => node.style.position = 'initial',
   };
 }
 
-export function fadeOut() {
+export function fadeOut(node) {
   return {
     duration,
     delay: 0,
     easing: sineOut,
     css: (t) => `opacity: ${t}`,
+    tick: () => node.style.position = 'fixed',
   };
 }
