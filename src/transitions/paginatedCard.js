c05cc9fbd39d4d6f32ad6dd00935e79872c492fa
Bryan Lee <bryanmylee@gmail.com>
2020-08-05 19:22:29 +0800

    Properly implement deferred transitions for paginated card

--- src/transitions/paginatedCard.js
+++ src/transitions/paginatedCard.js
@@ -10,7 +10,12 @@ export function getCardInOut({
   let rightCardWidth = 0;
   let rightCardHeight = 0;
 
+  let sending = false;
+  let receiving = false;
+
   function leftCardIn(node) {
+    receiving = true;
+
     const style = getComputedStyle(node);
     leftCardWidth = parseFloat(style.width);
     leftCardHeight = parseFloat(style.height);
@@ -19,42 +24,60 @@ export function getCardInOut({
     const deltaWidth = rightCardWidth - leftCardWidth;
     const deltaHeight = rightCardHeight - leftCardHeight;
 
-    return {
-      delay: bothDelay,
-      duration: bothDuration,
-      easing: bothEasing,
-      css: (t, u) => `
-        transform: ${transform} translate(${u * -leftCardWidth}px, 0);
-        opacity: ${targetOpacity * t};
-        width: ${rightCardWidth - deltaWidth * t}
-        max-height: ${rightCardHeight - deltaHeight * t}px;
-        min-height: ${rightCardHeight - deltaHeight * t}px;
-      `,
-      tick: () => node.style.position = 'initial',
+    return () => {
+      if (!sending) {
+        receiving = false;
+        return null;
+      }
+      sending = false;
+      return {
+        delay: bothDelay,
+        duration: bothDuration,
+        easing: bothEasing,
+        css: (t, u) => `
+          transform: ${transform} translate(${u * -leftCardWidth}px, 0);
+          opacity: ${targetOpacity * t};
+          width: ${rightCardWidth - deltaWidth * t}
+          max-height: ${rightCardHeight - deltaHeight * t}px;
+          min-height: ${rightCardHeight - deltaHeight * t}px;
+        `,
+        tick: () => node.style.position = 'initial',
+      };
     };
   }
 
-  function leftCardOut(node) {
+  function rightCardOut(node) {
+    sending = true;
+
     const style = getComputedStyle(node);
-    leftCardWidth = parseFloat(style.width);
-    leftCardHeight = parseFloat(style.height);
+    rightCardWidth = parseFloat(style.width);
+    rightCardHeight = parseFloat(style.height);
     const targetOpacity = +style.opacity;
     const transform = style.transform === 'none' ? '' : style.transform;
 
-    return {
-      delay: bothDelay,
-      duration: bothDuration,
-      easing: bothEasing,
-      css: (t, u) => `
-        transform: ${transform} translate(${u * -leftCardWidth}px, 0);
-        opacity: ${targetOpacity * t};
-        width: ${leftCardWidth}px;
-      `,
-      tick: () => node.style.position = 'fixed',
+    return () => {
+      if (!receiving) {
+        sending = false;
+        return null;
+      }
+      receiving = false;
+      return {
+        delay: bothDelay,
+        duration: bothDuration,
+        easing: bothEasing,
+        css: (t, u) => `
+          transform: ${transform} translate(${u * rightCardWidth}px, 0);
+          opacity: ${targetOpacity * t};
+          width: ${rightCardWidth}px;
+        `,
+        tick: () => node.style.position = 'fixed',
+      };
     };
   }
 
   function rightCardIn(node) {
+    receiving = true;
+
     const style = getComputedStyle(node);
     rightCardWidth = parseFloat(style.width);
     rightCardHeight = parseFloat(style.height);
@@ -63,38 +86,54 @@ export function getCardInOut({
     const deltaWidth = leftCardWidth - rightCardWidth;
     const deltaHeight = leftCardHeight - rightCardHeight;
 
-    return {
-      delay: bothDelay,
-      duration: bothDuration,
-      easing: bothEasing,
-      css: (t, u) => `
-        transform: ${transform} translate(${u * rightCardWidth}px, 0);
-        opacity: ${targetOpacity * t};
-        width: ${leftCardWidth - deltaWidth * t}px;
-        max-height: ${leftCardHeight - deltaHeight * t}px;
-        min-height: ${leftCardHeight - deltaHeight * t}px;
-      `,
-      tick: () => node.style.position = 'initial',
+    return () => {
+      if (!sending) {
+        receiving = false;
+        return null;
+      }
+      sending = false;
+      return {
+        delay: bothDelay,
+        duration: bothDuration,
+        easing: bothEasing,
+        css: (t, u) => `
+          transform: ${transform} translate(${u * rightCardWidth}px, 0);
+          opacity: ${targetOpacity * t};
+          width: ${leftCardWidth - deltaWidth * t}px;
+          max-height: ${leftCardHeight - deltaHeight * t}px;
+          min-height: ${leftCardHeight - deltaHeight * t}px;
+        `,
+        tick: () => node.style.position = 'initial',
+      };
     };
   }
 
-  function rightCardOut(node) {
+  function leftCardOut(node) {
+    sending = true;
+
     const style = getComputedStyle(node);
-    rightCardWidth = parseFloat(style.width);
-    rightCardHeight = parseFloat(style.height);
+    leftCardWidth = parseFloat(style.width);
+    leftCardHeight = parseFloat(style.height);
     const targetOpacity = +style.opacity;
     const transform = style.transform === 'none' ? '' : style.transform;
 
-    return {
-      delay: bothDelay,
-      duration: bothDuration,
-      easing: bothEasing,
-      css: (t, u) => `
-        transform: ${transform} translate(${u * rightCardWidth}px, 0);
-        opacity: ${targetOpacity * t};
-        width: ${rightCardWidth}px;
-      `,
-      tick: () => node.style.position = 'fixed',
+    return () => {
+      if (!receiving) {
+        sending = false;
+        return null;
+      }
+      receiving = false;
+      return {
+        delay: bothDelay,
+        duration: bothDuration,
+        easing: bothEasing,
+        css: (t, u) => `
+          transform: ${transform} translate(${u * -leftCardWidth}px, 0);
+          opacity: ${targetOpacity * t};
+          width: ${leftCardWidth}px;
+        `,
+        tick: () => node.style.position = 'fixed',
+      };
     };
   }
 
