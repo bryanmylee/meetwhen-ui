f4c2f58bd14d37843c3b45d34fad38c9d2619764
Bryan Lee <bryanmylee@gmail.com>
2020-08-05 15:03:31 +0800

    Fix up favicon on safari

--- src/routes/[eventUrl]/index.svelte
+++ src/routes/[eventUrl]/index.svelte
@@ -25,7 +25,9 @@
 
 <script>
   import { stores } from '@sapper/app';
+  import { onDestroy } from 'svelte';
 
+  import { colors } from 'src/utils/colors';
   import { formEnum, form } from './stores';
   import { undoRedo } from 'src/actions/hotkeys';
   import undoable from 'src/utils/undoable';
@@ -181,6 +183,8 @@
       errorMessage = err.message;
     }
   }
+
+  onDestroy(() => currentColor.setBaseColor(colors.blue500));
 </script>
 
 <svelte:window use:undoRedo={{ undo: selections.undo, redo: selections.redo }} />
