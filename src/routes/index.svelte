bd1266d4f3486611e7eb8841d2536bb6070dcd98
Bryan Lee <bryanmylee@gmail.com>
2021-01-16 22:35:34 +0800

    feat: update metadata

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,7 +30,7 @@
     class="p-4 text-white card bg-gradient-primary"
     >
     <h1 class="text-xl font-bold">meetwhen</h1>
-    <p>Find the perfect time to meet up with your team</p>
+    <p class="text-sm">Find the perfect time to meet up with your team</p>
   </div>
 
   <div class="p-4 card space-y-4">
