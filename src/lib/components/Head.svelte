78d0134627223ac1c8890501c0c02233b9d69240
Bryan Lee <bryanmylee@gmail.com>
2021-05-20 01:28:14 +0800

    feat: extract emoji for head component

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  export let emoji = '';
   export let subtitle = '';
 </script>
 
 <svelte:head>
-  <title>meetwhen.io {subtitle}</title>
+  <title>meetwhen.io {emoji} {subtitle}</title>
 </svelte:head>
---
+++ src/routes/new/index.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Head from '$lib/components/Head.svelte';
+</script>
+
+<Head emoji="✏️" subtitle="new event" />
