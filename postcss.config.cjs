d8c80c239af7afd52d2a0f04c6a75fb1cc0ecae0
Bryan Lee <bryanmylee@gmail.com>
2021-05-19 18:30:22 +0800

    build: configure postcss-nested

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,22 +1,18 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
 module.exports = {
   plugins: [
     // Some plugins, like postcss-nested, need to run before Tailwind
+    require('postcss-nested'),
 
-    tailwindcss,
+    require('tailwindcss'),
 
     // But others, like autoprefixer, need to run after
-
-    autoprefixer,
+    require('autoprefixer'),
 
     !dev &&
-      cssnano({
+      require('cssnano')({
         preset: 'default',
       }),
   ],
