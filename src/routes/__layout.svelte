525cf178ba40c7ac6c7001be4e8038f0c06edd56
Bryan Lee <bryanmylee@gmail.com>
2021-05-20 01:17:55 +0800

    feat: app crossfade context

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,27 @@
+<script lang="ts" context="module">
+  export type INewEventName = Writable<string>;
+  export type ICrossfade = Readable<ReturnType<typeof crossfade>>;
+</script>
+
 <script lang="ts">
   import '../app.postcss';
   import '$lib/colors';
+  import { setContext } from 'svelte';
+  import { crossfade, fade } from 'svelte/transition';
+  import { cubicOut } from 'svelte/easing';
+  import { Readable, derived, Writable, writable } from 'svelte/store';
+
+  setContext('new-event-name', writable(''));
+
+  const appCrossfade = crossfade({
+    duration: 500,
+    easing: cubicOut,
+    fallback: (node, params) => fade(node, { ...params, duration: 200 }),
+  });
+  setContext(
+    'crossfade',
+    derived(writable(appCrossfade), (x) => x)
+  );
 </script>
 
 <main>
