eeed2eecb69075de222cc5133a5737a12b522c0f
Bryan Lee <bryanmylee@gmail.com>
2021-05-20 23:20:30 +0800

    fix: build issue with custom classes

--- src/lib/components/Textfield.svelte
+++ src/lib/components/Textfield.svelte
@@ -8,25 +8,7 @@
   export { className as class };
 </script>
 
-<div class={className}>
+<div class="textfield {className}">
   <input {id} type="text" bind:value {required} class:filled={value !== ''} />
   <label for={id}>{placeholder}</label>
 </div>
-
-<style lang="postcss">
-  div {
-    @apply relative inline-block;
-    & > input[type='text'] {
-      @apply relative w-full p-4 pt-6 rounded-xl bg-gray-100 focusable;
-      &:focus,
-      &.filled {
-        & + label {
-          @apply text-xs -top-5 text-primary;
-        }
-      }
-    }
-    & > label {
-      @apply absolute -top-2 left-0 mt-7 mb-5 mx-4 text-gray-700 transition-all pointer-events-none;
-    }
-  }
-</style>
