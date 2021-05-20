727e023135abee0c90b7b326296f554eee3510e4
Bryan Lee <bryanmylee@gmail.com>
2021-05-20 16:55:59 +0800

    feat(temp): add simple fade in on page

--- src/lib/app-crossfade.ts
+++ src/lib/app-crossfade.ts
@@ -2,7 +2,7 @@ import { cubicOut } from 'svelte/easing';
 import { crossfade, fade } from 'svelte/transition';
 
 export const [send, receive] = crossfade({
-  duration: 10000,
+  duration: 500,
   easing: cubicOut,
   fallback: (node, params) => fade(node, { ...params, duration: 200 }),
 });
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,15 +9,17 @@
   import { writable } from 'svelte/store';
   import type { Writable } from 'svelte/store';
   import { page } from '$app/stores';
-  import PageTransition from '$lib/components/PageTransition.svelte';
   import Nav from './_nav.svelte';
+  import PageTransition from '$lib/components/PageTransition.svelte';
 
   setContext('new-event-name', writable(''));
 </script>
 
-<PageTransition key={$page.path}>
-  <slot />
-</PageTransition>
+<main>
+  <PageTransition key={$page.path}>
+    <slot />
+  </PageTransition>
+</main>
 
 {#if $page.path !== '/'}
   <Nav />
