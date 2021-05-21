17f05e7bf2a1b72604b8bbaf18690aff0e22036b
Bryan Lee <bryanmylee@gmail.com>
2021-05-22 02:08:42 +0800

    feat: provide disabled items for select

--- src/lib/components/Select/Select.svelte
+++ src/lib/components/Select/Select.svelte
@@ -20,6 +20,9 @@
 
   $: selected = items.length > 0 ? items[index] : null;
 
+  export let disabledItems: unknown[] = [];
+  const isDisabled = (item: unknown) => disabledItems.map(getId).includes(getId(item));
+
   const [ref, popper, getInstance] = createPopperActions<SelectModifiers>();
 
   let showDropdown = false;
@@ -45,13 +48,20 @@
     }
   };
 
-  const optionclick = (clickedIndex: number) => {
+  const optionclick = (clickedItem: unknown, clickedIndex: number) => {
+    if (isDisabled(clickedItem)) {
+      return;
+    }
     index = clickedIndex;
     showDropdown = false;
   };
 
   let refElement: HTMLElement | undefined;
   $: refHeight = refElement?.getBoundingClientRect().height ?? 0;
+
+  const disabledClass = 'text-gray-300 cursor-default';
+  const enabledClass =
+    'hover:z-10 hover:bg-gray-200 active:z-10 active:bg-gray-400 active:text-white';
 </script>
 
 <div
@@ -71,11 +81,10 @@
     >
       {#each items as item, itemIndex (getId(item))}
         <div
-          on:click={() => optionclick(itemIndex)}
+          on:click={() => optionclick(item, itemIndex)}
           class={cx(
-            'px-4 py-2 z-0 cursor-pointer focusable whitespace-nowrap',
-            'hover:z-10 hover:bg-gray-200',
-            'active:z-10 active:bg-gray-400 active:text-white',
+            'px-4 py-2 z-0 focusable whitespace-nowrap',
+            [isDisabled(item), disabledClass, enabledClass],
             [itemIndex === focusedIndex, 'bg-gray-200'],
             [itemIndex === index, '!bg-primary text-white']
           )}
