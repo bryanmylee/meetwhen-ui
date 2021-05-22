cc0cb5e93674cbc3ab1cf3bced5c4505a85a5d84
Bryan Lee <bryanmylee@gmail.com>
2021-05-22 17:27:40 +0800

    feat: update storage store api

--- src/lib/utils/local-storage-store.ts
+++ src/lib/utils/local-storage-store.ts
@@ -2,22 +2,30 @@ import type { Writable } from 'svelte/store';
 import { writable } from 'svelte/store';
 
 interface UseLocalOptions<T> {
-  initialValue?: T;
-  encode?: (value: T) => string;
-  decode?: (value: string) => T;
+  init?: T;
+  code?: Coder<T>;
 }
 
-export const useLocal = <T>(key: string, options?: UseLocalOptions<T>): Writable<T> => {
-  if (
-    !(typeof options?.initialValue === 'string') &&
-    !(options?.initialValue instanceof String) &&
-    (options?.encode === undefined || options?.decode === undefined)
-  ) {
-    throw new Error('An encoder and decoder must be provided for non-string data');
-  }
+type Encoder<T> = (value: T) => string;
+type Decoder<T> = (str: string) => T;
+type Coder<T> = [Encoder<T>, Decoder<T>];
 
-  const encode = options?.encode ?? ((value: T) => (value as unknown) as string);
-  const decode = options?.decode ?? ((value: string) => (value as unknown) as T);
+export const IDENTITY = <T>(): Coder<T> => [
+  (v: T): string => (v as unknown) as string,
+  (v: string): T => (v as unknown) as T,
+];
+export const STRING: Coder<string> = [(v: string): string => v, (v: string): string => v];
+export const NUMBER: Coder<number> = [
+  (v: number): string => v.toString(),
+  (v: string): number => parseFloat(v),
+];
+export const BOOLEAN: Coder<boolean> = [
+  (v: boolean): string => v.toString(),
+  (v: string): boolean => v === 'true',
+];
+
+export const useLocal = <T>(key: string, options?: UseLocalOptions<T>): Writable<T> => {
+  const [encode, decode] = options?.code ?? IDENTITY();
 
   const setLocalStorage = (value: T) => {
     if (typeof window === 'undefined') {
@@ -39,7 +47,7 @@ export const useLocal = <T>(key: string, options?: UseLocalOptions<T>): Writable
     return decode(result);
   };
 
-  const initialValue = options?.initialValue ?? getLocalStorage();
+  const initialValue = options?.init ?? getLocalStorage();
 
   const store = writable(initialValue, () => {
     if (typeof window === 'undefined') {
@@ -50,7 +58,8 @@ export const useLocal = <T>(key: string, options?: UseLocalOptions<T>): Writable
       window.removeEventListener('storage', handleStorageUpdate);
     };
   });
-  setLocalStorage(options?.initialValue);
+
+  setLocalStorage(options?.init);
 
   let currentValue: T;
   const handleStorageUpdate = () => {
