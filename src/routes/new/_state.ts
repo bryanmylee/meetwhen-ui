c8ae19f938374812907fb00c191be05a6ab866df
Bryan Lee <bryanmylee@gmail.com>
2021-05-28 21:06:50 +0800

    feat: fold intervals if adjacent

--- src/routes/new/_state.ts
+++ src/routes/new/_state.ts
@@ -1,3 +1,4 @@
+import type { Interval } from '$lib/gql/types/interval';
 import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 import { derived, writable } from 'svelte/store';
@@ -7,12 +8,13 @@ export const from = writable<Dayjs>(undefined);
 export const to = writable<Dayjs>(undefined);
 
 export const intervals = derived([selectedDates, from, to], ([$selectedDates, $from, $to]) => {
-  return $selectedDates.map((selectedDate) => getInterval(selectedDate, $from, $to));
+  const _intervals = $selectedDates.map((selectedDate) => getInterval(selectedDate, $from, $to));
+  return foldIntervals(_intervals);
 });
 
 const MIDNIGHT_TODAY = dayjs().startOf('day');
 
-const getInterval = (date: Dayjs, from: Dayjs, to: Dayjs) => {
+const getInterval = (date: Dayjs, from: Dayjs, to: Dayjs): Interval => {
   if (from === undefined || to === undefined) {
     return {
       beg: 0,
@@ -27,4 +29,25 @@ const getInterval = (date: Dayjs, from: Dayjs, to: Dayjs) => {
     beg: fromTimestamp.unix(),
     end: toTimestamp.unix(),
   };
+};
+
+const foldIntervals = (intervals: Interval[]): Interval[] => {
+  if (intervals.length <= 1) {
+    return intervals;
+  }
+  const result: Interval[] = [];
+  let previous = intervals[0];
+  intervals.slice(1).forEach((current) => {
+    if (previous.end === current.beg) {
+      previous = {
+        beg: previous.beg,
+        end: current.end,
+      };
+    } else {
+      result.push(previous);
+      previous = current;
+    }
+  });
+  result.push(previous);
+  return result;
 };
