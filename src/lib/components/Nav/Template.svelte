06d0d8eafe7d75fe88b5400dd0f71029ad8e9f1e
Bryan Lee <bryanmylee@gmail.com>
2021-05-30 03:03:48 +0800

    style: limit width of nav bar internal div

--- src/lib/components/Nav/Template.svelte
+++ src/lib/components/Nav/Template.svelte
@@ -6,25 +6,26 @@
 </script>
 
 {#if key === '/'}
-  <nav
-    transition:fly={{ y: -50 }}
-    class="fixed inset-0 bottom-auto flex items-center justify-end p-4 dark:text-white"
-  >
-    <ul class="flex items-center space-x-3">
-      <slot name="right" />
-    </ul>
+  <nav transition:fly={{ y: -50 }} class="fixed inset-0 bottom-auto">
+    <div class="flex items-center justify-end mx-auto max-w-7xl dark:text-white">
+      <ul class="flex items-center p-4 space-x-3">
+        <slot name="right" />
+      </ul>
+    </div>
   </nav>
 {:else}
   <nav
     in:receive={{ key: 'header' }}
     out:send={{ key: 'header' }}
-    class="fixed inset-0 bottom-auto flex items-center justify-between p-4 rounded-t-none card"
+    class="fixed inset-0 bottom-auto rounded-t-none card"
   >
-    <ul class="flex items-center space-x-3">
-      <slot name="left" />
-    </ul>
-    <ul class="flex items-center space-x-3">
-      <slot name="right" />
-    </ul>
+    <div class="flex items-center justify-between mx-auto max-w-7xl dark:text-white">
+      <ul class="flex items-center p-4 space-x-3">
+        <slot name="left" />
+      </ul>
+      <ul class="flex items-center p-4 space-x-3">
+        <slot name="right" />
+      </ul>
+    </div>
   </nav>
 {/if}
