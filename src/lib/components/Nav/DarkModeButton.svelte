be34ad40568f3a85513dc920534f803b388c771f
Bryan Lee <bryanmylee@gmail.com>
2021-05-30 03:24:24 +0800

    style: dismiss on click outside

--- src/lib/components/Nav/DarkModeButton.svelte
+++ src/lib/components/Nav/DarkModeButton.svelte
@@ -2,8 +2,9 @@
   import { slide } from 'svelte/transition';
   import { createPopperActions } from 'svelte-popperjs';
   import { isDark } from '$lib/dark-mode';
-  import { DropletIcon, MoonIcon, SunIcon } from 'svelte-feather-icons';
+  import { clickOutside } from '$lib/utils/use-click-outside';
   import { cx } from '$lib/utils/cx';
+  import { DropletIcon, MoonIcon, SunIcon } from 'svelte-feather-icons';
 
   const [ref, content] = createPopperActions({
     modifiers: [{ name: 'offset', options: { offset: [0, 10] } }],
@@ -27,21 +28,30 @@
   $: activeIcon = $isDark === undefined ? DropletIcon : $isDark ? MoonIcon : SunIcon;
 
   // prettier-ignore
-  const getClassname = (active: boolean) => cx(
+  const contentClassName = (active: boolean) => cx(
     'flex flex-col items-center w-16 p-2 space-y-2 rounded-lg button shade',
     [active, '!text-primary-darker dark:!text-primary']
   );
 </script>
 
-<button use:ref on:click={() => (showDropdown = !showDropdown)} class="w-5 h-5 button">
-  <svelte:component this={activeIcon} />
+<button use:ref on:click={() => (showDropdown = !showDropdown)} class="w-5 h-5">
+  <svelte:component
+    this={activeIcon}
+    class="hover:text-primary {cx([showDropdown, 'text-primary'])}"
+  />
 </button>
+
 {#if showDropdown}
-  <div use:content transition:slide|local={{ duration: 200 }} class="flex p-2 space-x-2 card">
+  <div
+    use:content
+    use:clickOutside={() => setTimeout(() => (showDropdown = false))}
+    transition:slide|local={{ duration: 200 }}
+    class="flex p-2 space-x-2 card"
+  >
     {#each options as { title, icon, value }}
       <button
         on:click={() => handleClick(value)}
-        class={getClassname(value === $isDark)}
+        class={contentClassName(value === $isDark)}
         disabled={value === $isDark}
       >
         <svelte:component this={icon} class="w-5 h-5 min-w-5 min-h-5" />
