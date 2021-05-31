17b8b378f6e9087cfac77acd492acc185429b63b
Bryan Lee <bryanmylee@gmail.com>
2021-05-31 23:38:26 +0800

    feat: zip

---
+++ src/lib/utils/zip.ts
@@ -0,0 +1,15 @@
+export const zip = <T1, T2>(arr1: T1[], arr2: T2[]): [T1, T2][] => {
+  const result: [T1, T2][] = [];
+  const minLength = Math.min(arr1.length, arr2.length);
+  let index = 0;
+  for (; index < minLength; index++) {
+    result.push([arr1[index], arr2[index]]);
+  }
+  for (; index < arr1.length; index++) {
+    result.push([arr1[index], null]);
+  }
+  for (; index < arr2.length; index++) {
+    result.push([null, arr2[index]]);
+  }
+  return result;
+};
