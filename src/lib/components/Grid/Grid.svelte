b163299378bcac7f9da94f2e5900c90f49b349d7
Bryan Lee <bryanmylee@gmail.com>
2021-05-31 20:38:01 +0800

    feat: css grid helper components

---
+++ src/lib/components/Grid/Grid.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { cssVars } from '$lib/utils/use-css-vars';
+
+  let className = '';
+  export { className as class };
+
+  export let rows = 1;
+  export let cols = 1;
+  $: vars = { rows, cols };
+</script>
+
+<div class="{className} grid" use:cssVars={vars}>
+  <slot />
+</div>
+
+<style>
+  div {
+    grid-template: repeat(var(--rows), 1fr) / repeat(var(--cols), 1fr);
+  }
+</style>
---
+++ src/lib/components/Grid/GridItem.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { cssVars } from '$lib/utils/use-css-vars';
+
+  let className = '';
+  export { className as class };
+
+  export let x = 0;
+  export let y = 0;
+  export let width = 1;
+  export let height = 1;
+  $: vars = {
+    rowStart: y + 1,
+    rowEnd: y + 1 + height,
+    colStart: x + 1,
+    colEnd: x + 1 + width,
+  };
+</script>
+
+<div class={className} use:cssVars={vars}>
+  <slot />
+</div>
+
+<style>
+  div {
+    grid-area: var(--rowStart) / var(--colStart) / var(--rowEnd) / var(--colEnd);
+  }
+</style>
