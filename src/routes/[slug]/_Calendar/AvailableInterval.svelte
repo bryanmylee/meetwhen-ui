c7b09278fdd144378da1109626058e25293ac3a5
Bryan Lee <bryanmylee@gmail.com>
2021-06-01 01:09:45 +0800

    style: available intervals

--- src/routes/[slug]/_Calendar/AvailableInterval.svelte
+++ src/routes/[slug]/_Calendar/AvailableInterval.svelte
@@ -13,17 +13,19 @@
   $: hours = getHoursInInterval(interval, $hourStepSize);
   $: props = hours.map($getItemPropsByTime);
 
-  const getClass = (endOfAvailable: boolean) => {
-    // prettier-ignore
+  const getClass = (index: number) => {
     return cx(
-      'shade min-w-32 min-h-10',
-      [endOfAvailable, 'mb-2', 'border-gray-200 dark:border-gray-600 border-b-2']
+      'shade min-w-32 min-h-5',
+      [index === 0, 'rounded-t-xl'],
+      [
+        index === hours.length - 1,
+        'rounded-b-xl mb-2',
+        cx([index % 2 === 1, 'border-gray-200 dark:border-gray-600 border-b-2']),
+      ]
     );
   };
 </script>
 
-{#each zip(hours, props) as [hour, prop]}
-  <GridItem {x} y={prop.rowIndex} class={getClass(prop.endOfAvailable)}>
-    {hour.hour}-{hour.minute}
-  </GridItem>
+{#each zip(hours, props) as [hour, prop], index}
+  <GridItem {x} y={prop.rowIndex} class={getClass(index)} />
 {/each}
