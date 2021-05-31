cd1faab0fae2a1fd580cf89c89884e4df88a57d7
Bryan Lee <bryanmylee@gmail.com>
2021-06-01 02:09:34 +0800

    fix: calendar grid size

--- src/routes/[slug]/_Calendar/Calendar.svelte
+++ src/routes/[slug]/_Calendar/Calendar.svelte
@@ -17,7 +17,7 @@
   <div tabindex="0" class="focus:outline-none" />
   <SelectableProvider bind:this={selector} bind:selectedIds let:selecting>
     <div class="relative h-full min-h-0 p-4 overflow-hidden focus:outline-none">
-      <div class="flex flex-col h-full overflow-auto">
+      <div class="h-full overflow-auto">
         <div class="sticky top-0 z-20 flex w-full space-x-4">
           {#each $days as day}
             <ColumnHeader {day} />
@@ -26,7 +26,7 @@
         <Grid
           rows={$numRows}
           cols={$days.length}
-          class="relative z-0 min-w-full min-h-full gap-x-4"
+          class="relative z-0 min-w-full cal-grid-size gap-x-4"
         >
           {#each $days as day, index}
             <Column x={index} {day} />
@@ -36,3 +36,9 @@
     </div>
   </SelectableProvider>
 </div>
+
+<style lang="postcss">
+  :global(.cal-grid-size) {
+    min-height: calc(100% - 4rem);
+  }
+</style>
