beb45269f7cfbaf94099da183dad8d9ba987f2d3
Bryan Lee <bryanmylee@gmail.com>
2021-06-03 04:16:13 +0800

    feat: hour labels to cap off each available section

--- src/routes/[slug]/_Calendar/IndexColumnAvailable.svelte
+++ src/routes/[slug]/_Calendar/IndexColumnAvailable.svelte
@@ -4,27 +4,20 @@
   import { getHoursInTimeInterval } from './utils';
   import { getRowIndexByTime, hourStepSize, totalHours } from './state';
   import GridItem from '$lib/components/Grid/GridItem.svelte';
-  import type { Time } from '$lib/utils/time';
-  import { cx } from '$lib/utils/cx';
 
   export let available: LocalTimeInterval;
   $: hours = getHoursInTimeInterval(available, $hourStepSize);
   $: rowIndices = hours.map($getRowIndexByTime);
-  $: lastHour = hours[hours.length - 1]?.add($hourStepSize, 'hour');
-
-  const getClass = (hour: Time, index: number) => {
-    const isLastInColumn = $totalHours[$totalHours.length - 1].unix === hour.unix;
-    const isLastInAvailable = index === hours.length - 1;
-    const lastClass = cx([!isLastInColumn, 'mb-4']);
-    // prettier-ignore
-    return cx(
-      'flex items-center justify-end flex-1 pr-4 transform -translate-y-1/2',
-      [isLastInAvailable, lastClass]);
-  };
+  $: endHour = hours[hours.length - 1].add($hourStepSize, 'hour');
+  $: isEndHourLastInColumn =
+    $totalHours[$totalHours.length - 1].unix === hours[hours.length - 1].unix;
 </script>
 
-{#each zip(hours, rowIndices) as [hour, rowIndex], index}
-  <GridItem y={rowIndex} class={getClass(hour, index)}>
+{#each zip(hours, rowIndices) as [hour, rowIndex]}
+  <GridItem
+    y={rowIndex}
+    class="flex items-center justify-end flex-1 pr-4 transform -translate-y-1/2"
+  >
     {#if hour.minute === 0}
       <span class="text-xs text-gray-400">
         {hour.format('ha')}
@@ -33,10 +26,23 @@
   </GridItem>
 {/each}
 
-<!--GridItem y={rowIndices[rowIndices.length - 1]} class="flex items-end justify-end flex-1 pr-4">
-  {#if lastHour?.minute === 0}
-    <span class="text-xs text-gray-400">
-      {lastHour?.format('ha')}
-    </span>
-  {/if}
-</GridItem-->
+{#if !isEndHourLastInColumn}
+  <GridItem
+    y={rowIndices[rowIndices.length - 1]}
+    class="flex items-end justify-end flex-1 pr-4 transform -translate-y-1/2"
+  >
+    {#if endHour.minute === 0}
+      <span class="text-xs text-gray-400">
+        {endHour.format('ha')}
+      </span>
+    {/if}
+  </GridItem>
+{:else}
+  <div class="absolute bottom-0 right-0 flex items-end justify-end pr-4">
+    {#if endHour.minute === 0}
+      <span class="text-xs text-gray-400">
+        {endHour.format('ha')}
+      </span>
+    {/if}
+  </div>
+{/if}
