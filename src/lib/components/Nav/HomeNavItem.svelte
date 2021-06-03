06caa7561b4b39087ded23550d267e477f137a6d
Bryan Lee <bryanmylee@gmail.com>
2021-06-03 22:01:16 +0800

    fix: gradient on home nav item

--- src/lib/components/Nav/HomeNavItem.svelte
+++ src/lib/components/Nav/HomeNavItem.svelte
@@ -1,10 +1,10 @@
-<li class="relative pointer-events-none">
+<li class="relative z-0 pointer-events-none">
   <span class="text-xl font-bold text-transparent bg-clip-text bg-gradient-primary">
     meetwhen
   </span>
   <a
     href="/"
-    class="absolute inset-0 text-xl font-bold pointer-events-auto -z-1 text-primary hover:underline"
+    class="absolute inset-0 text-xl font-bold pointer-events-auto z-[-1] text-primary hover:underline"
   >
     meetwhen
   </a>
