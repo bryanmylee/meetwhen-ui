8912fb42d62643423b650778de4be499c6883b4d
Bryan Lee <bryanmylee@gmail.com>
2021-06-03 19:32:00 +0800

    feat: update buttons on auth

--- src/routes/[slug]/_Buttons.svelte
+++ src/routes/[slug]/_Buttons.svelte
@@ -1,14 +1,17 @@
 <script>
-  import { showAuth } from '$lib/app-state';
+  import { isLoggedIn, showAuth } from '$lib/app-state';
+  $: label = $isLoggedIn ? 'Join' : 'Join as Guest';
 </script>
 
 <div class="flex space-x-4">
-  <button
-    type="button"
-    on:click={() => ($showAuth = true)}
-    class="w-full p-3 button shade rounded-xl"
-  >
-    Login
-  </button>
-  <button type="submit" class="w-full p-3 button gradient rounded-xl"> Join as Guest </button>
+  {#if !$isLoggedIn}
+    <button
+      type="button"
+      on:click={() => ($showAuth = true)}
+      class="w-full p-3 button shade rounded-xl"
+    >
+      Login
+    </button>
+  {/if}
+  <button type="submit" class="w-full p-3 button primary rounded-xl">{label}</button>
 </div>
