fd4a6ddaf1898537d552ac410e65ef38b1cdf1ac
Bryan Lee <bryanmylee@gmail.com>
2021-06-09 02:53:13 +0800

    feat: union of schedules

--- src/routes/[slug]/_Calendar/utils/schedules.ts
+++ src/routes/[slug]/_Calendar/utils/schedules.ts
@@ -1,13 +1,81 @@
-import type { Schedule, User } from '$lib/gql/types';
+import type { Interval, Schedule, User } from '$lib/gql/types';
 import type { Moment } from '$lib/utils/moment';
+import dayjs from 'dayjs';
+import { Map, Set } from 'immutable';
+
+type UserMap = Map<string, unknown>;
 
 interface ScheduleMoment extends Moment {
-  users: Pick<User, 'id' | 'name' | 'email' | 'isGuest'>[];
+  user: UserMap;
 }
 
-export const unionSchedules = (schedules: Schedule[]) => {
+interface IntervalWithUsersSerial {
+  beg: number;
+  end: number;
+  users: Set<UserMap>;
+}
+
+type ShallowUser = Pick<User, 'id' | 'name' | 'isGuest'>;
+
+interface IntervalWithUsers extends Interval {
+  users: ShallowUser[];
+}
+
+export const unionSchedules = (schedules: Schedule[]): IntervalWithUsers[] => {
   const moments: ScheduleMoment[] = [];
-  schedules.forEach((schedule) => {
-    schedule.user.id;
+  schedules.forEach(({ user, intervals }) => {
+    intervals.forEach(({ beg, end }) => {
+      moments.push(
+        {
+          end: false,
+          unix: beg.unix(),
+          user: Map(user),
+        },
+        {
+          end: true,
+          unix: end.unix(),
+          user: Map(user),
+        }
+      );
+    });
   });
+  moments.sort((a, b) => {
+    if (a.unix === b.unix) {
+      // order ending moments first.
+      return a.end === true ? -1 : 1;
+    }
+    return a.unix - b.unix;
+  });
+
+  const intervals: IntervalWithUsersSerial[] = [];
+  let currentBeg: number = null;
+  let currentUsers: Set<UserMap> = Set();
+  moments.forEach((moment) => {
+    if (moment.end) {
+      intervals.push({
+        beg: currentBeg,
+        end: moment.unix,
+        users: currentUsers,
+      });
+      currentBeg = moment.unix;
+      currentUsers = currentUsers.remove(moment.user);
+    } else {
+      intervals.push({
+        beg: currentBeg,
+        end: moment.unix,
+        users: currentUsers,
+      });
+      currentBeg = moment.unix;
+      currentUsers = currentUsers.add(moment.user);
+    }
+  });
+  const filteredIntervals = intervals.filter(
+    ({ beg, end, users }) => beg !== end && users.size > 0
+  );
+  const renderedIntervals: IntervalWithUsers[] = filteredIntervals.map(({ beg, end, users }) => ({
+    beg: dayjs.unix(beg),
+    end: dayjs.unix(end),
+    users: users.toArray().map((userMap) => userMap.toObject() as ShallowUser),
+  }));
+  return renderedIntervals;
 };
