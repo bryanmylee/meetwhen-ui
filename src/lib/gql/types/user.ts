b72fc7141e1fb6278a977c0ce7e8ab511f299df1
Bryan Lee <bryanmylee@gmail.com>
2021-06-09 03:16:54 +0800

    feat: provide shallow user type

--- src/lib/gql/types/user.ts
+++ src/lib/gql/types/user.ts
@@ -1,18 +1,18 @@
 import type { Meeting, MeetingDTO, Schedule, ScheduleDTO } from '.';
 import type { Identifiable } from './identifiable';
 
-export interface User extends Identifiable {
+export interface ShallowUser extends Identifiable {
   name: string;
   email: string;
   isGuest: boolean;
+}
+
+export interface User extends ShallowUser {
   meetings: Partial<Meeting>[];
   schedules: Partial<Schedule>[];
 }
 
-export interface UserDTO extends Identifiable {
-  name: string;
-  email: string;
-  isGuest: boolean;
+export interface UserDTO extends ShallowUser {
   meetings: Partial<MeetingDTO>[];
   schedules: Partial<ScheduleDTO>[];
 }
