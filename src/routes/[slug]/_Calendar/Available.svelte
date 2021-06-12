00aa8f545ead183ecfc8be32cd262ee43bdbc48d
Bryan Lee <bryanmylee@gmail.com>
2021-06-13 01:05:13 +0800

    feat: show enabled / disabled status

--- src/routes/[slug]/_Calendar/Available.svelte
+++ src/routes/[slug]/_Calendar/Available.svelte
@@ -3,12 +3,26 @@
   import type { Dayjs } from 'dayjs';
   import { getIntervalsInAvailableByDay } from './state/intervals';
   import AvailableCells from './AvailableCells.svelte';
+  import { getContext } from 'svelte';
+  import type { Writable } from 'svelte/store';
+  import Interval from './Interval.svelte';
+  import { cx } from '$lib/utils/cx';
 
   export let day: Dayjs;
   export let available: LocalTimeInterval;
   $: intervals = $getIntervalsInAvailableByDay(available, day);
+
+  const disabled = getContext<Writable<boolean>>('disabled');
+
+  // prettier-ignore
+  $: intervalClass = cx(
+    'shadow-lg pointer-events-none rounded-xl border-3 border-primary-lighter dark:shadow-md-primary transition-all',
+    [$disabled, 'opacity-0'],
+  );
 </script>
 
 {#each intervals as interval}
   <AvailableCells {day} {interval} />
+  <!-- For indicating enabled/disabled status -->
+  <Interval {interval} class={intervalClass} />
 {/each}
