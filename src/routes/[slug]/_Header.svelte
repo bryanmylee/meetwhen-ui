6c5a2c2869f7843a3ee4fa8dd743a4af0b9cb84a
Bryan Lee <bryanmylee@gmail.com>
2021-06-14 17:09:22 +0800

    feat: hide header when adding schedule

--- src/routes/[slug]/_Header.svelte
+++ src/routes/[slug]/_Header.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
+  import { guestUser, modalState } from './_state/page';
+  import type { ModalState } from './_state/page';
+
   export let name = '';
+
+  const statesWithTextfield: ModalState[] = ['add-auth', 'add-guest'];
 </script>
 
-<div class="p-4 text-white card bg-gradient-primary">
-  {name}&nbsp;
-</div>
+{#if !statesWithTextfield.includes($modalState)}
+  <div class="p-4 text-white card bg-gradient-primary">
+    <p class="text-lg font-medium">
+      {name}&nbsp;
+    </p>
+    {#if $guestUser !== null}
+      <p class="text-xs italic text-white">
+        Logged in as guest <strong>{$guestUser.name}</strong>
+      </p>
+    {/if}
+  </div>
+{/if}
