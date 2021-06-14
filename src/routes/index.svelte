cf2c8d98da9299225dc9ff46984f6908d27e1076
Bryan Lee <bryanmylee@gmail.com>
2021-06-14 19:00:15 +0800

    refactor: rename event to meeting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import Head from '$lib/components/Head.svelte';
   import Header from './_Header.svelte';
-  import NewEvent from './_NewEvent.svelte';
-  import FindEvent from './_FindEvent.svelte';
+  import NewMeeting from './_NewMeeting.svelte';
+  import FindMeeting from './_FindMeeting.svelte';
 </script>
 
 <Head />
 
 <div class="max-w-lg p-6 mx-auto space-y-4">
   <Header />
-  <NewEvent />
-  <FindEvent />
+  <NewMeeting />
+  <FindMeeting />
 </div>
