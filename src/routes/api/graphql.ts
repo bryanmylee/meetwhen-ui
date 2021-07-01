7252d10a0c7f3cf591605af9696a36732ad569e6
Bryan Lee <bryanmylee@gmail.com>
2021-07-01 23:02:01 +0800

    fix: forwarding access token cookie as auth for backend

--- src/routes/api/graphql.ts
+++ src/routes/api/graphql.ts
@@ -2,12 +2,10 @@ import { env } from '$lib/env';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const post: RequestHandler = async (request) => {
+  const cookies = getCookies(request.headers.cookie);
   const response = await fetch(env.VITE_API_GQL_ENDPOINT, {
     method: 'post',
-    headers: {
-      'content-type': 'application/json',
-      accept: 'application/json',
-    },
+    headers: getBackendHeaders(cookies),
     body: request.rawBody,
   });
   const body = await response.json();
@@ -16,6 +14,23 @@ export const post: RequestHandler = async (request) => {
     headers: getClientHeaders(request.host, response),
     body,
   };
+};
+
+const getCookies = (rawCookie: string): Record<string, string> => {
+  return Object.fromEntries(
+    rawCookie.split('; ').map((rawCookieEntry: string) => rawCookieEntry.split('='))
+  );
+};
+
+const getBackendHeaders = (cookies: Record<string, string>): Record<string, string> => {
+  const headers = {
+    'content-type': 'application/json',
+    accept: 'application/json',
+  };
+  if (cookies['access-token'] !== undefined) {
+    headers['authorization'] = `Bearer ${cookies['access-token']}`;
+  }
+  return headers;
 };
 
 const getClientHeaders = (host: string, response: Response) => {
