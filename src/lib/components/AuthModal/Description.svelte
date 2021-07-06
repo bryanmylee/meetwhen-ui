c6e6b19ae2be6476d6ab8990edacd31f4a0daca7
Bryan Lee <bryanmylee@gmail.com>
2021-07-06 11:41:38 +0800

    style: highlight this meeting only

--- src/lib/components/AuthModal/Description.svelte
+++ src/lib/components/AuthModal/Description.svelte
@@ -3,12 +3,15 @@
 
   export let activeMeeting: Meeting;
   export let isGuestAuth = false;
-
-  const spanClass = '';
 </script>
 
 {#if isGuestAuth}
-  <p class="text-sm">Your account will only be used for this meeting</p>
+  <p class="text-sm">
+    Your account will be used for
+    <span class="font-bold text-transparent bg-clip-text bg-gradient-primary">
+      this meeting only
+    </span>
+  </p>
   <div class="card p-4 text-white bg-gradient-primary">
     {activeMeeting.name}
   </div>
