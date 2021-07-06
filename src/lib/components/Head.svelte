a9bc73efaf7ba417c1a64a01c6f2c79697c7c052
Bryan Lee <bryanmylee@gmail.com>
2021-07-07 01:31:27 +0800

    fix: remove unnecessary head meta elements

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -6,9 +6,7 @@
 
 <svelte:head>
   <title>meetwhen.io {emoji} {subtitle}</title>
-  <meta charset="utf-8" />
   <meta name="description" content="Find the perfect time to meet up with your team" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   {#if noRobots}
     <meta name="robots" content="noindex" />
   {/if}
