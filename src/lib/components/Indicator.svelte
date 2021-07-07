a1466c43711674ad8b1f05c6150694233717df9d
Bryan Lee <bryanmylee@gmail.com>
2021-07-07 17:05:41 +0800

    style: fade indicator out after trigger

--- src/lib/components/Indicator.svelte
+++ src/lib/components/Indicator.svelte
@@ -26,17 +26,16 @@
   id="indicator"
   class={classes([
     'fixed w-28 h-28 rounded-full pointer-events-none bg-primary-fifty',
-    !expanding && 'opacity-0',
-    triggered && 'border-4 border-primary-darker dark:border-white',
+    triggered && 'border-4 border-primary-darker dark:border-white opacity-0',
   ])}
   class:expanding
-  style="left: {x}px; top: {y}px; --triggerDelay: {triggerDelay}ms"
+  style="left: {x}px; top: {y}px; --triggerDelay: {triggerDelay}ms; --triggerDuration: {triggerDuration}ms;"
 />
 
 <style lang="postcss">
   div {
     transform: translate(-50%, -50%) scale(0.25);
-    transition: transform var(--triggerDelay) ease-out, opacity var(--triggerDelay) ease-out;
+    transition: transform var(--triggerDelay) ease-out, opacity var(--triggerDuration) ease-out;
   }
 
   div.expanding {
