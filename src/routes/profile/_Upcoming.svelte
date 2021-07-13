d4575d437277fbfc532ec76a3e0e9549ae5f0458
Bryan Lee <bryanmylee@gmail.com>
2021-07-14 03:11:18 +0800

    style: remove extra element when empty

--- src/routes/profile/_Upcoming.svelte
+++ src/routes/profile/_Upcoming.svelte
@@ -2,14 +2,16 @@
   import MeetingItem from './_MeetingItem.svelte';
   import type { ShallowMeeting } from '$lib/gql/types';
 
-  export let upcomingMeetings: ShallowMeeting[];
+  export let upcomingMeetings: ShallowMeeting[] = [];
 </script>
 
 <div class="p-4 space-y-4 card">
   <h1 class="text-lg font-medium">Upcoming meetings</h1>
-  <ul class="space-y-4">
-    {#each upcomingMeetings as meeting}
-      <MeetingItem {meeting} />
-    {/each}
-  </ul>
+  {#if upcomingMeetings.length > 0}
+    <ul class="space-y-4">
+      {#each upcomingMeetings as meeting}
+        <MeetingItem {meeting} />
+      {/each}
+    </ul>
+  {/if}
 </div>
