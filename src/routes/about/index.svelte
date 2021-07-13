1fce9de700a7fac81a28f07f4aba44374fcbc19e
Bryan Lee <bryanmylee@gmail.com>
2021-07-13 21:54:22 +0800

    feat: add author in footer

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,25 +1,31 @@
-<script>
+<script lang="ts">
   import { isDark } from '$lib/app-state';
-
-  const highlightedClass = 'font-bold text-transparent bg-clip-text bg-gradient-primary';
 </script>
 
 <article class="max-w-lg p-6 mx-auto space-y-4">
   <section class="card p-4 space-y-2">
     <h1 class="text-lg font-medium">About</h1>
     <p>
-      <span class={highlightedClass}>meetwhen</span> makes group meetings quick and easy!
+      <span class="font-bold text-gradient-primary">meetwhen</span> makes group meetings quick and easy!
     </p>
   </section>
   <section class="card p-4 space-y-2">
     <h2 class="font-medium">Create meetings</h2>
-    <img src="/about/new_{$isDark ? 'dark' : 'light'}.png" alt="Create a new meet" />
   </section>
+  <img src="/about/new_{$isDark ? 'dark' : 'light'}.png" alt="Create a new meet" />
   <section class="card p-4 space-y-2">
     <h2 class="font-medium">Join your friends</h2>
-    <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
   </section>
+  <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
 </article>
+<footer class="mt-4 max-w-lg mx-auto px-6">
+  <a href="https://bryanmylee.github.io/" class="block card p-4 rounded-b-none text-sm">
+    <p class="text-center">
+      Proudly developed by
+      <span class="font-bold text-gradient-primary"> Bryan Lee </span>
+    </p>
+  </a>
+</footer>
 
 <style lang="postcss">
   img {
