be040d4e3714755880b9711aa55aaf55c5b65120
Bryan Lee <bryanmylee@gmail.com>
2021-07-14 03:00:10 +0800

    refactor: help section

---
+++ src/routes/about/_Help.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { isDark } from '$lib/app-state';
+  import Accordian from '$lib/components/Accordian.svelte';
+  import { ChevronDownIcon } from 'svelte-feather-icons';
+</script>
+
+<Accordian
+  xclass={() => 'dark:text-white select-none'}
+  xtitleclass={() =>
+    'flex justify-between w-full p-4 card focus:outline-none focus:ring ring-inset ring-primary-lighter'}
+  let:expanded
+>
+  <svelte:fragment slot="title">
+    <h1 class="text-lg font-medium">Help</h1>
+    <ChevronDownIcon class="p-2 -m-2 w-10 h-10 transition transform {expanded && 'rotate-180'}" />
+  </svelte:fragment>
+  <h2>Create meetings</h2>
+  <img src="/about/new_{$isDark ? 'dark' : 'light'}.gif" alt="Create a new meet" />
+  <h2>Join your friends</h2>
+  <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
+  <h2>Share with others</h2>
+  <img src="/about/meeting_share_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
+</Accordian>
+
+<style lang="postcss">
+  h2 {
+    @apply font-medium px-4 pt-4 my-2 border-t border-gray-400;
+    &:first-of-type {
+      @apply border-t-0;
+    }
+  }
+
+  img {
+    @apply rounded-xl;
+  }
+</style>
--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,38 +1,21 @@
 <script lang="ts">
-  import { isDark } from '$lib/app-state';
+  import Help from './_Help.svelte';
 </script>
 
-<article class="max-w-lg p-6 mx-auto space-y-4">
+<article class="max-w-lg p-6 pb-4 mx-auto space-y-4">
   <section class="card p-4 space-y-2">
     <h1 class="text-lg font-medium">About</h1>
     <p>
       <span class="font-bold text-gradient-primary">meetwhen</span> makes group meetings quick and easy!
     </p>
   </section>
-  <section class="card p-4 space-y-2">
-    <h2 class="font-medium">Create meetings</h2>
-  </section>
-  <img src="/about/new_{$isDark ? 'dark' : 'light'}.gif" alt="Create a new meet" />
-  <section class="card p-4 space-y-2">
-    <h2 class="font-medium">Join your friends</h2>
-  </section>
-  <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
-  <section class="card p-4 space-y-2">
-    <h2 class="font-medium">Share with others</h2>
-  </section>
-  <img src="/about/meeting_share_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
+  <Help />
 </article>
-<footer class="mt-4 max-w-lg mx-auto px-6">
-  <a href="https://bryanmylee.github.io/" class="block card px-4 pt-6 pb-8 rounded-b-none text-sm">
+<footer class="max-w-lg px-6 pb-4 mx-auto">
+  <a href="https://bryanmylee.github.io/" class="block card p-4 text-sm hover:ring ring-primary">
     <p class="text-center">
       Proudly developed by
       <span class="font-bold text-gradient-primary"> Bryan Lee </span>
     </p>
   </a>
 </footer>
-
-<style lang="postcss">
-  img {
-    @apply rounded-xl;
-  }
-</style>
