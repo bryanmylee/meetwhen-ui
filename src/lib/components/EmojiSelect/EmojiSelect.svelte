a50a4c62642dbcbe7f4f6a468baa9e87a422641e
Bryan Lee <bryanmylee@gmail.com>
2021-07-14 21:49:37 +0800

    feat: show advanced settings

--- src/lib/components/EmojiSelect/EmojiSelect.svelte
+++ src/lib/components/EmojiSelect/EmojiSelect.svelte
@@ -1,47 +1,30 @@
 <script lang="ts">
   import { clickOutside } from '$lib/utils/actions/use-click-outside';
-  import { createPopperActions } from 'svelte-popperjs';
   import EmojiPicker from './EmojiPicker.svelte';
-
-  const [ref, content, getInstance] = createPopperActions({
-    strategy: 'absolute',
-    placement: 'top-end',
-  });
 
   export let value = '📘';
 
-  let isShowingPicker = false;
-
-  const togglePicker = () => {
-    showPicker ? showPicker() : hidePicker();
-  };
-
-  const showPicker = () => {
-    isShowingPicker = true;
-    setTimeout(() => {
-      getInstance()?.update();
-    }, 16);
-  };
-
-  const hidePicker = () => {
-    isShowingPicker = false;
-  };
+  let showPicker = false;
 
   let className = '';
   export { className as class };
 </script>
 
-<button
-  type="button"
-  use:ref
-  tabindex="0"
-  on:click={togglePicker}
-  class="{className} px-4 py-2 shade focusable rounded-xl cursor-pointer"
->
-  {value}
-</button>
-{#if isShowingPicker}
-  <div use:content use:clickOutside={() => setTimeout(() => hidePicker(), 50)}>
-    <EmojiPicker bind:value />
-  </div>
-{/if}
+<div class="relative">
+  <button
+    type="button"
+    tabindex="0"
+    on:click={() => (showPicker = !showPicker)}
+    class="{className} text-lg px-4 py-2 shade focusable rounded-xl cursor-pointer"
+  >
+    {value}
+  </button>
+  {#if showPicker}
+    <div
+      use:clickOutside={() => setTimeout(() => (showPicker = false), 17)}
+      class="absolute right-0 bottom-full"
+    >
+      <EmojiPicker bind:value />
+    </div>
+  {/if}
+</div>
--- src/routes/new/_AdvancedSettings.svelte
+++ src/routes/new/_AdvancedSettings.svelte
@@ -4,6 +4,13 @@
   let emoji: string;
 </script>
 
-{emoji}
-
-<EmojiSelect bind:value={emoji} />
+<div class="card p-4 space-y-4">
+  <div class="flex justify-between items-center flex-1">
+    <span>Pick a color</span>
+    <input type="color" />
+  </div>
+  <div class="flex justify-between items-center flex-1">
+    <span>Set a custom emoji</span>
+    <EmojiSelect bind:value={emoji} />
+  </div>
+</div>
