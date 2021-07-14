3851bec4c34118061e2ac157467ebd43e5f3f9d6
Bryan Lee <bryanmylee@gmail.com>
2021-07-14 19:33:21 +0800

    fix: position of picker

--- src/lib/components/EmojiSelect/EmojiSelect.svelte
+++ src/lib/components/EmojiSelect/EmojiSelect.svelte
@@ -3,26 +3,45 @@
   import { createPopperActions } from 'svelte-popperjs';
   import EmojiPicker from './EmojiPicker.svelte';
 
-  const [ref, content] = createPopperActions({
-    placement: 'top',
+  const [ref, content, getInstance] = createPopperActions({
+    strategy: 'absolute',
+    placement: 'top-end',
   });
 
   export let value = '📘';
 
-  let showPicker = true;
+  let isShowingPicker = false;
+
+  const togglePicker = () => {
+    showPicker ? showPicker() : hidePicker();
+  };
+
+  const showPicker = () => {
+    isShowingPicker = true;
+    setTimeout(() => {
+      getInstance()?.update();
+    }, 16);
+  };
+
+  const hidePicker = () => {
+    isShowingPicker = false;
+  };
+
+  let className = '';
+  export { className as class };
 </script>
 
 <button
   type="button"
   use:ref
   tabindex="0"
-  on:click={() => (showPicker = !showPicker)}
-  class="px-4 py-2 shade focusable rounded-xl cursor-pointer"
+  on:click={togglePicker}
+  class="{className} px-4 py-2 shade focusable rounded-xl cursor-pointer"
 >
   {value}
 </button>
-{#if showPicker}
-  <div use:content use:clickOutside={() => setTimeout(() => (showPicker = false), 50)}>
+{#if isShowingPicker}
+  <div use:content use:clickOutside={() => setTimeout(() => hidePicker(), 50)}>
     <EmojiPicker bind:value />
   </div>
 {/if}
