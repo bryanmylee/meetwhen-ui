8e47c61d3eb0a18e43faf508a4ce55a3406ecea3
Bryan Lee <bryanmylee@gmail.com>
2021-07-14 11:49:21 +0800

    perf: remove accordian to allow preloading of images

--- src/routes/about/_Help.svelte
+++ src/routes/about/_Help.svelte
@@ -1,29 +1,15 @@
 <script lang="ts">
-  import { slide } from 'svelte/transition';
   import { isDark } from '$lib/app-state';
-  import Accordian from '$lib/components/Accordian.svelte';
-  import { ChevronDownIcon } from 'svelte-feather-icons';
 </script>
 
-<Accordian
-  xclass={() => 'dark:text-white select-none'}
-  xtitleclass={() =>
-    'flex justify-between w-full p-4 card focus:outline-none focus:ring ring-inset ring-primary-lighter'}
-  let:expanded
->
-  <svelte:fragment slot="title">
-    <h1 class="text-lg font-medium">Help</h1>
-    <ChevronDownIcon class="p-2 -m-2 w-10 h-10 transition transform {expanded && 'rotate-180'}" />
-  </svelte:fragment>
-  <div transition:slide|local class="flex flex-col items-center">
-    <h2>Create meetings</h2>
-    <img src="/about/new_{$isDark ? 'dark' : 'light'}.gif" alt="Create a new meet" />
-    <h2>Join your friends</h2>
-    <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
-    <h2>Share with others</h2>
-    <img src="/about/meeting_share_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
-  </div>
-</Accordian>
+<div class="flex flex-col items-center">
+  <h2>Create meetings</h2>
+  <img src="/about/new_{$isDark ? 'dark' : 'light'}.gif" alt="Create a new meet" />
+  <h2>Join your friends</h2>
+  <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
+  <h2>Share with others</h2>
+  <img src="/about/meeting_share_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
+</div>
 
 <style lang="postcss">
   h2 {
