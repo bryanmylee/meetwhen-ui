493be64ad82a644d6b549756ad190abfd39f22d3
Bryan Lee <bryanmylee@gmail.com>
2021-07-28 22:12:28 +0800

    fix: prevent user scalable

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -6,6 +6,10 @@
 
 <svelte:head>
   <title>meetwhen {emoji} {subtitle}</title>
+  <meta
+    name="viewport"
+    content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+  />
   <meta name="description" content="Find the perfect time to meet up with your team" />
   {#if noRobots}
     <meta name="robots" content="noindex" />
