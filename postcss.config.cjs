ff0654d2bded68cdf5ad223f126e2c8cc1d20640
Bryan Lee <bryanmylee@gmail.com>
2021-07-29 15:25:50 +0800

    fix: tailwind nesting and remove recursive class definition

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,13 +4,10 @@ const dev = mode === 'development';
 module.exports = {
   plugins: [
     // Some plugins, like postcss-nested, need to run before Tailwind
-    require('postcss-nested'),
-
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
-
     // But others, like autoprefixer, need to run after
     require('autoprefixer'),
-
     !dev &&
       require('cssnano')({
         preset: 'default',
