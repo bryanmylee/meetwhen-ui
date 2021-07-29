f67f3c26229bee4df5e183a751690e7b6cb79c0e
Bryan Lee <bryanmylee@gmail.com>
2021-07-29 12:03:31 +0800

    fix: use anchor instead of button

--- src/routes/profile/_MeetingItem.svelte
+++ src/routes/profile/_MeetingItem.svelte
@@ -13,17 +13,16 @@
   $: colorVars = getCssVars('primary', getColorSet(meeting.color));
 </script>
 
-<li
-  tabindex="0"
-  on:keydown={onSubmitKey(navigate)}
-  on:click={navigate}
-  class="relative button cursor-pointer rounded-lg overflow-hidden shade"
-  style={colorVars}
->
-  <div class="absolute left-0 w-2 h-full bg-primary" />
-  <div class="flex p-2 space-x-2 ml-2 items-center font-normal">
-    <div>
-      {meeting.name}&nbsp;
+<li tabindex="0" style={colorVars}>
+  <a
+    href="/{meeting.slug}"
+    class="block relative button cursor-pointer rounded-lg overflow-hidden shade"
+  >
+    <div class="absolute left-0 w-2 h-full bg-primary" />
+    <div class="flex p-2 space-x-2 ml-2 items-center font-normal">
+      <div>
+        {meeting.name}&nbsp;
+      </div>
     </div>
-  </div>
+  </a>
 </li>
