99e9e643aeb49b4d9a2433ce24033a4bb0b378d1
Bryan Lee <bryanmylee@gmail.com>
2021-07-29 15:29:58 +0800

    fix: remove tabindex 0 for meeting item

--- src/routes/profile/_MeetingItem.svelte
+++ src/routes/profile/_MeetingItem.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
   import type { ShallowMeeting } from '$lib/gql/types';
-  import { onSubmitKey } from '$lib/utils/on-submit-key';
   import { getCssVars, getColorSet } from '$lib/utils/stores/colors-store';
 
   export let meeting: ShallowMeeting;
 
-  const navigate = () => {
-    goto(`/${meeting.slug}`);
-  };
-
   $: colorVars = getCssVars('primary', getColorSet(meeting.color));
 </script>
 
-<li tabindex="0" style={colorVars}>
+<li style={colorVars}>
   <a
     href="/{meeting.slug}"
     class="block relative button cursor-pointer rounded-lg overflow-hidden shade"
