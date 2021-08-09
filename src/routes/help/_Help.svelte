6f17606f0fdb7dc385bdea48717bccaa511e2fac
Bryan Lee <bryanmylee@gmail.com>
2021-08-09 15:55:34 +0800

    feat: improve help page headings

--- src/routes/help/_Help.svelte
+++ src/routes/help/_Help.svelte
@@ -3,20 +3,20 @@
 </script>
 
 <div class="flex flex-col items-center dark:text-white">
-  <h2>Create meetings</h2>
+  <h2>Create a new meeting</h2>
   <img src="/about/new_{$isDark ? 'dark' : 'light'}.gif" alt="Create a new meet" />
-  <h2>Join your friends</h2>
+  <h2>Join a meeting with others</h2>
   <img src="/about/meeting_joining_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
-  <h2>Share with others</h2>
+  <h2>Share your meeting link</h2>
   <img src="/about/meeting_share_{$isDark ? 'dark' : 'light'}.gif" alt="Join a meet" />
 </div>
 
 <style lang="postcss">
   h2 {
-    @apply font-medium px-4 pt-4 mt-2;
+    @apply text-xl font-bold px-4 pt-4 my-2;
   }
 
   img {
-    @apply rounded-xl;
+    @apply rounded-xl mb-8;
   }
 </style>
--- src/routes/help/index.svelte
+++ src/routes/help/index.svelte
@@ -6,17 +6,11 @@
 <Head subtitle="help" emoji="🔬" />
 
 <article class="max-w-lg p-6 pb-4 mx-auto space-y-4">
-  <section class="card p-4 space-y-2">
-    <h1 class="text-lg font-medium">About</h1>
-    <p>
-      <span class="font-bold text-gradient-primary">meetwhen</span> makes group meetings quick and easy!
-    </p>
-  </section>
   <Help />
 </article>
 <footer class="max-w-lg px-6 pb-4 mx-auto">
   <a href="https://bryanmylee.github.io/" class="block card p-4 text-sm hover:ring ring-primary">
-    <p class="text-center">
+    <p class="text-center text-base italic">
       Proudly developed by
       <span class="font-bold text-gradient-primary"> Bryan Lee </span>
     </p>
