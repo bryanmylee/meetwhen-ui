22fa27b74539c4867a0e19a3db30bb8dd1b99e20
Bryan Lee <bryanmylee@gmail.com>
2021-12-01 17:25:09 +0800

    fix: white on hover

--- src/lib/components/meetings/molecues/MeetingCard.svelte
+++ src/lib/components/meetings/molecues/MeetingCard.svelte
@@ -25,7 +25,7 @@
 			aria-label="Share"
 			on:keydown|stopPropagation={onSubmitKey(() => (showShareModal = true))}
 			on:click|preventDefault|stopPropagation={() => (showShareModal = true)}
-			class="w-10 h-10 -m-2 rounded-full button hover:bg-primary-darker focus:ring-primary-text"
+			class="w-10 h-10 -m-2 rounded-full button hover:text-white hover:bg-primary-darker focus:ring-primary-text"
 		>
 			<ShareIcon class="p-2.5" />
 		</button>
--- src/routes/loading/index.svelte
+++ src/routes/loading/index.svelte
@@ -11,18 +11,18 @@
 </script>
 
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { get } from 'svelte/store';
-	import { goto } from '$app/navigation';
-	import { loadingMeetingPromise, newMeeting } from '$lib/app-state';
-	import { ScaleOut } from 'svelte-loading-spinners';
-	import Head from '$lib/components/atoms/Head.svelte';
 	import Buttons from '../[slug]/_Buttons.svelte';
 	import Calendar from '../[slug]/_Calendar/Calendar.svelte';
-	import MeetingCard from '$lib/components/atoms/MeetingCard.svelte';
+	import Head from '$lib/components/atoms/Head.svelte';
+	import MeetingCard from '$lib/components/meetings/molecues/MeetingCard.svelte';
 	import Template from '../[slug]/_Template.svelte';
 	import type { Load } from '@sveltejs/kit';
 	import type { ShallowMeeting } from '$lib/gql/types';
+	import { ScaleOut } from 'svelte-loading-spinners';
+	import { get } from 'svelte/store';
+	import { goto } from '$app/navigation';
+	import { loadingMeetingPromise, newMeeting } from '$lib/app-state';
+	import { onMount } from 'svelte';
 
 	onMount(async () => {
 		$newMeeting = await $loadingMeetingPromise;
