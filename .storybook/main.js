7f3cff165e2dd9f194368dcb045184cc89394f59
Bryan Lee <bryanmylee@gmail.com>
2022-01-04 22:11:18 +0800

    feat: dark mode storybook previews

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,6 +18,7 @@ module.exports = {
 				},
 			},
 		},
+		'storybook-dark-mode',
 	],
 	framework: '@storybook/svelte',
 	svelteOptions: {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,4 +8,9 @@ export const parameters = {
 			date: /Date$/,
 		},
 	},
+	darkMode: {
+		darkClass: 'dark',
+		classTarget: 'html',
+		stylePreview: true,
+	},
 };
