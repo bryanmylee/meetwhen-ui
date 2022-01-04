8988554b51101944c065aabd9fd3e34f24623e32
Bryan Lee <bryanmylee@gmail.com>
2022-01-05 01:55:32 +0800

    doc: clean up story

--- src/lib/input/atoms/textfield/Textfield.stories.svelte
+++ src/lib/input/atoms/textfield/Textfield.stories.svelte
@@ -3,58 +3,33 @@
 	import Textfield from './Textfield.svelte';
 </script>
 
-<Meta title="Input/Textfield" component={Textfield} />
+<Meta
+	title="Input/Textfield"
+	component={Textfield}
+	argTypes={{
+		label: { control: 'text' },
+		id: { control: 'text' },
+	}}
+/>
 
 <Template let:args>
 	<Textfield {...args} />
 </Template>
 
-<Story
-	name="Empty"
-	args={{
-		label: 'Name',
-	}}
-/>
+<Story name="Gallery">
+	<div class="flex flex-col gap-4 w-52">
+		<Textfield label="Default" />
+		<Textfield label="Password" password />
+		<Textfield label="Error" error="Incorrect password" />
+		<Textfield label="Required" required />
+		<Textfield label="Disabled" disabled />
+	</div>
+</Story>
 
 <Story
-	name="Filled"
+	name="Custom"
+	source
 	args={{
-		label: 'Name',
-		value: 'Adam Smith',
-	}}
-/>
-
-<Story
-	name="Password"
-	args={{
-		label: 'Password',
-		value: 'password',
-		password: true,
-	}}
-/>
-
-<Story
-	name="Error"
-	args={{
-		label: 'Username',
-		value: 'adam',
-		error: 'Username already taken',
-	}}
-/>
-
-<Story
-	name="Required"
-	args={{
-		label: 'Date of birth',
-		required: true,
-	}}
-/>
-
-<Story
-	name="Disabled"
-	args={{
-		label: 'Promo code',
-		value: '1234',
-		disabled: true,
+		label: 'Custom',
 	}}
 />
