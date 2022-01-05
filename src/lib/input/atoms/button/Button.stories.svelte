2882a53b1ee41942ee565c9949ef153dc0b274b6
Bryan Lee <bryanmylee@gmail.com>
2022-01-05 12:23:26 +0800

    feat: button component

---
+++ src/lib/input/atoms/button/Button.stories.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+</script>
+
+<Meta
+	title="Input/Button"
+	component={Button}
+	argTypes={{
+		onClick: { action: 'onClick' },
+		onDoubleClick: { action: 'onDoubleClick' },
+		onContextMenu: { action: 'onContextMenu' },
+		onFocus: { action: 'onFocus' },
+		onBlur: { action: 'onBlur' },
+	}}
+/>
+
+<Template let:args>
+	<Button
+		{...args}
+		on:click={args.onClick}
+		on:dblclick={args.onDoubleClick}
+		on:contextmenu={args.onContextMenu}
+		on:focus={args.onFocus}
+		on:blur={args.onBlur}
+	>
+		Custom
+	</Button>
+</Template>
+
+<Story name="Gallery">
+	<div class="flex flex-col items-center gap-4">
+		<Button>Primary</Button>
+		<Button disabled>Disabled</Button>
+		<Button variant="outlined">Outline</Button>
+		<Button variant="outlined" disabled>Disabled Outline</Button>
+	</div>
+</Story>
+
+<Story name="Custom" source />
---
+++ src/lib/input/atoms/button/Button.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	export let type: 'button' | 'menu' | 'reset' | 'submit' = 'button';
+	export let disabled = false;
+	export let variant: 'primary' | 'outlined' = 'primary';
+
+	let className = '';
+	export { className as class };
+</script>
+
+<button
+	{type}
+	{disabled}
+	on:click
+	on:dblclick
+	on:contextmenu
+	on:focus
+	on:blur
+	class="button focus {className}"
+	class:primary={variant === 'primary'}
+	class:outlined={variant === 'outlined'}
+>
+	<slot />
+</button>
+
+<style lang="postcss" global>
+	.button {
+		@apply p-4 rounded-full font-bold;
+		@apply ring-offset-2 ring-offset-white dark:ring-offset-neutral-700;
+		@apply transition-colors;
+
+		&:disabled {
+			@apply text-neutral-400 cursor-default;
+		}
+
+		&.primary {
+			@apply bg-blue-400 text-white;
+			&:disabled {
+				@apply bg-opacity-50;
+			}
+			&:not(:disabled) {
+				&:hover {
+					@apply bg-blue-300 shadow-sm shadow-blue-300;
+				}
+				&:active {
+					@apply bg-blue-500 shadow shadow-blue-300;
+				}
+			}
+		}
+
+		&.outlined {
+			@apply border-2 border-blue-400 text-blue-400;
+			&:disabled {
+				@apply text-opacity-50 border-opacity-50;
+			}
+			&:not(:disabled) {
+				&:hover {
+					@apply text-blue-300 border-blue-300 shadow-sm shadow-blue-300;
+				}
+				&:active {
+					@apply text-blue-500 border-blue-500 shadow shadow-blue-300;
+				}
+			}
+		}
+	}
+</style>
