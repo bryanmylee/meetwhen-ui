d9a35939411a609dc68e01259ad2fbb1c65cf2ce
Bryan Lee <bryanmylee@gmail.com>
2022-01-06 15:51:00 +0800

    doc: selectable interpolate

--- src/lib/input/selectable/SelectableProvider.stories.svelte
+++ src/lib/input/selectable/SelectableProvider.stories.svelte
@@ -1,11 +1,20 @@
 <script>
 	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import { range } from '$lib/core/utils/range';
 	import SelectableProvider from './SelectableProvider.svelte';
 
 	/**
 	 * @type {string[]}
 	 */
 	let selectedIds = [];
+
+	const idsBetween = (/** @type {string} */ a, /** @type {string} */ b) => {
+		const _a = parseInt(a);
+		const _b = parseInt(b);
+		return range(Math.min(_a, _b), Math.max(_a, _b) + 1).map((i) =>
+			i.toString(),
+		);
+	};
 </script>
 
 <Meta
@@ -18,7 +27,6 @@
 
 <Story name="Sample selectable items" source let:args>
 	<h1 class="mb-4 text-xl font-bold">Selectable sample</h1>
-	<p>{selectedIds.join(', ')}&nbsp;</p>
 	<SelectableProvider
 		bind:selectedIds
 		{...args}
@@ -27,6 +35,34 @@
 		let:isIdSelected
 		let:isIdDisabled
 		let:isIdActive
+	>
+		<div class="flex flex-wrap gap-4">
+			{#each [...Array(20)] as _, id}
+				<div
+					data-id={id}
+					class="item"
+					class:selected={isIdSelected(`${id}`)}
+					aria-disabled={isIdDisabled(`${id}`)}
+					class:active={isIdActive(`${id}`)}
+				>
+					{id}
+				</div>
+			{/each}
+		</div>
+	</SelectableProvider>
+</Story>
+
+<Story name="Interpolate betweens" source let:args>
+	<h1 class="mb-4 text-xl font-bold">Interpolate between start and end</h1>
+	<SelectableProvider
+		bind:selectedIds
+		{...args}
+		disabledIds={['0', '1']}
+		on:toggle={args.onToggle}
+		let:isIdSelected
+		let:isIdDisabled
+		let:isIdActive
+		interpolate={idsBetween}
 	>
 		<div class="flex flex-wrap gap-4">
 			{#each [...Array(20)] as _, id}
@@ -58,5 +94,8 @@
 	}
 	.item.active {
 		@apply shadow-lg;
+	}
+	.item.active.selected {
+		@apply bg-primary-300;
 	}
 </style>
