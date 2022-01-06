d81060f9099a69b4f3cbce5d3a6f02a86f3c793d
Bryan Lee <bryanmylee@gmail.com>
2022-01-06 14:57:44 +0800

    refactor: use Maybe type

--- src/lib/core/utils/useJsEnabled/useJsEnabled.ts
+++ src/lib/core/utils/useJsEnabled/useJsEnabled.ts
@@ -1,9 +1,10 @@
 import { getContext, onMount } from 'svelte';
 import { readable } from 'svelte/store';
 import type { Readable } from 'svelte/store';
+import type { Maybe } from '$lib/core/types/Maybe';
 
 export const useJsEnabled = (): Readable<boolean> => {
-	const mockedJsEnabled = getContext<boolean | undefined>('jsEnabled');
+	const mockedJsEnabled = getContext<Maybe<boolean>>('jsEnabled');
 	return readable<boolean>(mockedJsEnabled ?? false, (set) => {
 		onMount(() => {
 			if (mockedJsEnabled === undefined) {
