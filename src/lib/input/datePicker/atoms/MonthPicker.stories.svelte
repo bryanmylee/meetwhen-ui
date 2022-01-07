b418b77b3a92bc1be11dde27abda727401ce51a9
Bryan Lee <bryanmylee@gmail.com>
2022-01-07 12:28:42 +0800

    feat: month picker

---
+++ src/lib/input/datePicker/atoms/MonthPicker.stories.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import dayjs from 'dayjs';
+	import MonthPicker from './MonthPicker.svelte';
+
+	let month = dayjs();
+</script>
+
+<Meta title="Input/DatePicker/MonthPicker" component={MonthPicker} />
+
+<Story name="Sample month picker" let:args>
+	<h1 class="mb-4 text-xl font-bold dark:text-white">Sample month picker</h1>
+	<MonthPicker bind:month {...args} />
+</Story>
---
+++ src/lib/input/datePicker/atoms/MonthPicker.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import type { Dayjs } from 'dayjs';
+	import { ChevronLeftIcon, ChevronRightIcon } from 'svelte-feather-icons';
+	import { Button } from '$lib/input';
+
+	export let month: Dayjs;
+	let today = dayjs();
+
+	$: isCurrentMonth = month.isSame(today, 'month');
+	$: isCurrentYear = month.isSame(today, 'year');
+
+	$: monthLabel = month.format(isCurrentYear ? 'MMMM' : 'MMMM YYYY');
+
+	const nextMonth = () => {
+		month = month.add(1, 'month');
+	};
+
+	const prevMonth = () => {
+		if (isCurrentMonth) {
+			return;
+		}
+		month = month.subtract(1, 'month');
+	};
+</script>
+
+<div class="flex items-center justify-between">
+	<Button
+		type="button"
+		disabled={isCurrentMonth}
+		on:click={prevMonth}
+		class="w-10 h-10 !p-2"
+		aria-label="previous month"
+	>
+		<ChevronLeftIcon />
+	</Button>
+	<span role="status" class="text-lg font-bold">
+		{monthLabel}
+	</span>
+	<Button
+		type="button"
+		aria-label="next month"
+		class="w-10 h-10 !p-2"
+		on:click={nextMonth}
+	>
+		<ChevronRightIcon />
+	</Button>
+</div>
