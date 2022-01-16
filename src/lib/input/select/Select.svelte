d97835fcee09179fb9b483a8a7a8df9eb19f059d
Bryan Lee <bryanmylee@gmail.com>
2022-01-17 02:46:48 +0800

    style: thinner select with dropdown icon

--- src/lib/input/select/Select.svelte
+++ src/lib/input/select/Select.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-	import { classes } from '$lib/core/utils/classes';
 	import {
 		Listbox,
 		ListboxButton,
 		ListboxOption,
 		ListboxOptions,
 	} from '@rgossiaux/svelte-headlessui';
+	import { ChevronDownIcon } from 'svelte-feather-icons';
+	import { classes } from '$lib/core/utils/classes';
 
 	type T = $$Generic;
 
@@ -27,8 +28,14 @@
 	class="listbox"
 	{disabled}
 >
-	<ListboxButton class={classes('listbox-button focus-inset', top && 'top')}>
-		{label(selectedValue)}
+	<ListboxButton
+		class={classes('listbox-button focus-inset', top && 'top')}
+		let:open
+	>
+		<span>
+			{label(selectedValue)}
+		</span>
+		<ChevronDownIcon class={classes('wh-6', open && 'rotate-180')} />
 	</ListboxButton>
 	<ListboxOptions class={classes('listbox-options focus-inset', top && 'top')}>
 		{#each values as value (id(value))}
@@ -46,10 +53,11 @@
 		@apply relative w-full;
 
 		& .listbox-button {
-			@apply p-4 bg-shade-100 rounded-xl w-full;
-			@apply text-left;
+			@apply px-4 py-2 bg-shade-100 rounded-xl w-full;
+			@apply text-left flex justify-between items-center;
 			@apply transition-colors;
 			&[aria-expanded='true'] {
+				@apply bg-shade-200;
 				&:not(.top) {
 					@apply rounded-b-none;
 				}
@@ -60,7 +68,7 @@
 			&:disabled {
 				@apply text-neutral-400 opacity-50;
 			}
-			&:not(:disabled) {
+			&:not(:disabled)[aria-expanded='false'] {
 				&:not(:active):hover {
 					@apply bg-shade-50 shadow;
 				}
@@ -75,10 +83,10 @@
 			@apply p-3 bg-shade-0 rounded-xl shadow-lg;
 			@apply max-h-96 overflow-auto;
 			&:not(.top) {
-				@apply top-14 rounded-t-none;
+				@apply top-10 rounded-t-none;
 			}
 			&.top {
-				@apply bottom-14 rounded-b-none;
+				@apply bottom-10 rounded-b-none;
 			}
 
 			& .listbox-option {
@@ -92,7 +100,7 @@
 				}
 				&.selected {
 					@apply bg-primary-400 text-white;
-					&:hover {
+					&:not(:active):hover {
 						@apply bg-primary-500;
 					}
 				}
