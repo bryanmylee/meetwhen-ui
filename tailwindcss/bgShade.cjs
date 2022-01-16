994be2a2dfcf58cb9eda21dc4fac24bdadd32f51
Bryan Lee <bryanmylee@gmail.com>
2022-01-17 01:41:19 +0800

    style: `bg-shade-0`

--- tailwindcss/bgShade.cjs
+++ tailwindcss/bgShade.cjs
@@ -1,14 +1,14 @@
 const plugin = require('tailwindcss/plugin');
 
 const bgShadePlugin = plugin(({ addUtilities, theme }) => {
-	const grades = [50, 100, 200, 300, 400, 500, 600, 700];
+	const grades = [0, 50, 100, 200, 300, 400, 500, 600, 700, 800];
 	const neutrals = theme('colors.neutral', {});
 	const shadeUtilities = {};
 	for (let i = 0; i < grades.length; i++) {
 		const lightKey = grades[i];
 		const darkKey = grades[grades.length - 1 - i];
 		shadeUtilities[`.bg-shade-${lightKey}`] = {
-			backgroundColor: neutrals[lightKey],
+			backgroundColor: neutrals[lightKey] ?? 'white',
 			':global(.dark) &': {
 				backgroundColor: neutrals[darkKey],
 			},
