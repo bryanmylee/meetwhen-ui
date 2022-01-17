0428ae183f21fd409ec700ad9057e50e38a6443f
Bryan Lee <bryanmylee@gmail.com>
2022-01-17 11:58:21 +0800

    fix (style): specificity

--- src/lib/input/select/Select.svelte
+++ src/lib/input/select/Select.svelte
@@ -20,24 +20,23 @@
 
 	export let disabled = false;
 	export let top = false;
+
+	export let size: 'sm' | 'lg' = 'sm';
 </script>
 
 <Listbox
 	value={selectedValue}
 	on:change={(event) => (selectedValue = event.detail)}
-	class="listbox"
+	class={classes('listbox', top && 'top', size)}
 	{disabled}
 >
-	<ListboxButton
-		class={classes('listbox-button focus-inset', top && 'top')}
-		let:open
-	>
+	<ListboxButton class="listbox-button focus-inset" let:open>
 		<span>
 			{label(selectedValue)}
 		</span>
 		<ChevronDownIcon class={classes('wh-6', open && 'rotate-180')} />
 	</ListboxButton>
-	<ListboxOptions class={classes('listbox-options focus-inset', top && 'top')}>
+	<ListboxOptions class="listbox-options focus-inset">
 		{#each values as value (id(value))}
 			<ListboxOption {value} let:active let:selected class="contents">
 				<div class="listbox-option" class:active class:selected>
@@ -51,59 +50,55 @@
 <style lang="postcss" global>
 	.listbox {
 		@apply relative w-full;
+	}
 
-		& .listbox-button {
-			@apply px-4 py-2 bg-shade-100 rounded-xl w-full;
-			@apply text-left flex justify-between items-center;
-			@apply transition-colors;
-			&[aria-expanded='true'] {
-				@apply bg-shade-200;
-				&:not(.top) {
-					@apply rounded-b-none;
-				}
-				&.top {
-					@apply rounded-t-none;
-				}
-			}
-			&:disabled {
-				@apply text-neutral-400 opacity-50;
-			}
-			&:not(:disabled)[aria-expanded='false'] {
-				&:not(:active):hover {
-					@apply bg-shade-50 shadow;
-				}
-				&:active {
-					@apply bg-shade-200 shadow-sm;
-				}
+	.listbox-button {
+		@apply px-4 py-2 bg-shade-100 rounded-xl w-full;
+		@apply text-left flex justify-between items-center;
+		@apply transition-colors;
+		&[aria-expanded='true'] {
+			@apply bg-shade-200 rounded-b-none;
+			.top & {
+				@apply rounded-t-none rounded-b-xl;
 			}
 		}
-
-		& .listbox-options {
-			@apply absolute left-0 right-0 z-10;
-			@apply p-3 bg-shade-0 rounded-xl shadow-lg;
-			@apply max-h-96 overflow-auto;
-			&:not(.top) {
-				@apply top-10 rounded-t-none;
+		&:disabled {
+			@apply text-neutral-400 opacity-50;
+		}
+		&:not(:disabled)[aria-expanded='false'] {
+			&:not(:active):hover {
+				@apply bg-shade-50 shadow;
 			}
-			&.top {
-				@apply bottom-10 rounded-b-none;
+			&:active {
+				@apply bg-shade-200 shadow-sm;
 			}
+		}
+	}
 
-			& .listbox-option {
-				@apply p-2 rounded-lg;
-				@apply whitespace-nowrap;
-				&:not(:active):hover {
-					@apply bg-shade-100;
-				}
-				&.active {
-					@apply bg-shade-200;
-				}
-				&.selected {
-					@apply bg-primary-400 text-white;
-					&:not(:active):hover {
-						@apply bg-primary-500;
-					}
-				}
+	.listbox-options {
+		@apply absolute left-0 right-0 z-10;
+		@apply p-3 bg-shade-0 rounded-xl shadow-lg;
+		@apply max-h-96 overflow-auto;
+		@apply top-10 rounded-t-none;
+		.top & {
+			@apply top-auto rounded-t-xl;
+			@apply bottom-10 rounded-b-none;
+		}
+	}
+
+	.listbox-option {
+		@apply p-2 rounded-lg cursor-pointer;
+		@apply whitespace-nowrap;
+		&:not(:active):hover {
+			@apply bg-shade-100;
+		}
+		&.active {
+			@apply bg-shade-200;
+		}
+		&.selected {
+			@apply bg-primary-400 text-white;
+			&:not(:active):hover {
+				@apply bg-primary-500;
 			}
 		}
 	}
