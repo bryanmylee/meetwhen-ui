c19895a0126fd8df3ff6c793fc7bdbcd1b469f8b
Bryan Lee <bryanmylee@gmail.com>
2022-01-19 01:57:05 +0800

    fix: prevent firebase re-init

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -2,7 +2,12 @@ import { readable } from 'svelte/store';
 import type { Readable } from 'svelte/store';
 import { initializeApp } from 'firebase/app';
 import type { FirebaseApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import {
+	browserSessionPersistence,
+	getAuth,
+	onAuthStateChanged,
+	setPersistence,
+} from 'firebase/auth';
 import type { Auth, User } from 'firebase/auth';
 import { pairedContext } from '$lib/core/utils/pairedContext';
 import type { Maybe } from '$lib/core/types/Maybe';
@@ -18,13 +23,22 @@ export const { get: getFirebaseUser, set: setFirebaseUser } =
 	pairedContext<Readable<Maybe<User>>>();
 
 export const initFirebaseContext = (): void => {
+	// Prevent re-initializing the Firebase SDK during hot reloading.
+	if (getFirebaseApp()) {
+		return;
+	}
+
 	const app = initializeApp(firebaseConfig);
+
 	const auth = getAuth(app);
+	setPersistence(auth, browserSessionPersistence);
+
 	const user = readable<Maybe<User>>(undefined, (set) => {
 		return onAuthStateChanged(auth, ($user) => {
 			set($user ?? undefined);
 		});
 	});
+
 	setFirebaseApp(app);
 	setFirebaseAuth(auth);
 	setFirebaseUser(user);
