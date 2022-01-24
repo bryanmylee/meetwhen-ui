dda4f32471d0edb723c1937abf3a18b1982886b3
Bryan Lee <bryanmylee@gmail.com>
2022-01-24 16:00:26 +0800

    refactor: parse meeting in function

---
+++ src/lib/firebase/queries/findMeetingWithSlug.ts
@@ -0,0 +1,27 @@
+import type { Id } from '$lib/core/types/Id';
+import type { Maybe } from '$lib/core/types/Maybe';
+import { MeetingConverter } from '$lib/models/Meeting';
+import type { Meeting, MeetingData } from '$lib/models/Meeting';
+import {
+	collection,
+	getDocs,
+	query,
+	QuerySnapshot,
+	where,
+} from 'firebase/firestore';
+import type { Firestore } from 'firebase/firestore';
+
+export const findMeetingWithSlug = async (
+	repo: Firestore,
+	slug: string,
+): Promise<Maybe<Id<Meeting>>> => {
+	const meetingSnapshot = (await getDocs(
+		query(collection(repo, 'meeting'), where('slug', '==', slug)),
+	)) as QuerySnapshot<MeetingData>;
+	if (meetingSnapshot.docs.length !== 1) {
+		return undefined;
+	}
+	const doc = meetingSnapshot.docs[0];
+	const meeting = MeetingConverter.parse(doc.data());
+	return { ...meeting, id: doc.id } as Id<Meeting>;
+};
--- src/routes/[slug]/index.svelte
+++ src/routes/[slug]/index.svelte
@@ -4,13 +4,10 @@
 		session,
 	}) => {
 		const { slug } = params;
-		const meetingData = await findMeetingDataWithSlug(
-			firebaseClient.repo,
-			slug,
-		);
+		const meeting = await findMeetingWithSlug(firebaseClient.repo, slug);
 		return {
 			props: {
-				meetingData,
+				meeting,
 				currentUser: session.user,
 			},
 		};
@@ -21,31 +18,30 @@
 	import type { Load } from '@sveltejs/kit';
 	import type { Session } from '$lib/core/types/Session';
 	import { MeetingConverter } from '$lib/models/Meeting';
-	import type { MeetingData } from '$lib/models/Meeting';
+	import type { Meeting, MeetingData } from '$lib/models/Meeting';
 	import type { Id } from '$lib/core/types/Id';
 	import type { SafeUser } from '$lib/models/SafeUser';
 	import { firebaseClient } from '$lib/firebase/client';
 	import { useRepo } from '$lib/firebase/context';
 	import { useLiveDocument } from '$lib/firebase/utils/useLiveDocument';
-	import { findMeetingDataWithSlug } from '$lib/firebase/queries/findMeetingDataWithSlug';
+	import { findMeetingWithSlug } from '$lib/firebase/queries/findMeetingWithSlug';
 
 	const repo = useRepo();
 
-	export let meetingData: Id<MeetingData>;
+	export let meeting: Id<Meeting>;
 	const liveMeetingDocument = useLiveDocument<MeetingData>(
 		repo,
 		'meeting',
-		meetingData.id,
+		meeting.id,
 	);
 	$: {
 		const data = $liveMeetingDocument?.data();
 		if (data !== undefined)
-			meetingData = {
-				id: meetingData.id,
-				...data,
+			meeting = {
+				id: meeting.id,
+				...MeetingConverter.parse(data),
 			};
 	}
-	$: meeting = MeetingConverter.parse(meetingData);
 
 	export let currentUser: SafeUser;
 </script>
