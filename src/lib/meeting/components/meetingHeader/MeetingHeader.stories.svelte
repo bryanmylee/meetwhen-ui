be26ce950be160b3045a71475eb7b3d58afc7cd2
Bryan Lee <bryanmylee@gmail.com>
2022-02-06 01:00:54 +0800

    doc: meeting header story

---
+++ src/lib/meeting/components/meetingHeader/MeetingHeader.stories.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import MeetingHeader from './MeetingHeader.svelte';
+</script>
+
+<Meta
+	title="Page/Meeting/MeetingHeader"
+	component={MeetingHeader}
+	argTypes={{
+		name: { control: 'text' },
+		slug: { control: 'text' },
+	}}
+/>
+
+<Template let:args>
+	<MeetingHeader name="custom meeting" slug="my_slug" {...args} />
+</Template>
+
+<Story name="Custom" source />
