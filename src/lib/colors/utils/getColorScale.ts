d7e0c907b22fed800ad5bcb42561a2f97e9a2b2f
Bryan Lee <bryanmylee@gmail.com>
2022-02-07 20:29:22 +0800

    fix: optional isDark prop for color scale

--- src/lib/colors/utils/getColorScale.ts
+++ src/lib/colors/utils/getColorScale.ts
@@ -1,7 +1,11 @@
 import chroma from 'chroma-js';
 import type { Color } from 'chroma-js';
 
-export type ColorScale = (numerator: number, denominator: number) => Color;
+export type ColorScale = (
+	numerator: number,
+	denominator: number,
+	isDark?: boolean,
+) => Color;
 
 const ratioWithMin = (ratio: number, min: number) => {
 	return ratio * (1 - min) + min;
@@ -11,13 +15,13 @@ const ratioWithMax = (ratio: number, max: number) => {
 	return ratio * max;
 };
 
-export const getColorScale = (hex: string, isDark = false): ColorScale => {
+export const getColorScale = (hex: string): ColorScale => {
 	const color = chroma(hex);
 	const light = color.brighten(1).desaturate(0.5);
 	const dark = color.darken(2);
-	const scalePoints = isDark ? [dark, color, light] : [light, color, dark];
-	const scale = chroma.scale(scalePoints).mode('lrgb');
-	return (numerator, denominator) => {
+	return (numerator, denominator, isDark = false) => {
+		const scalePoints = isDark ? [dark, color, light] : [light, color, dark];
+		const scale = chroma.scale(scalePoints).mode('lrgb');
 		const mostIndex = ratioWithMin(Math.min(1, denominator / 10), 0.5);
 		const index = ratioWithMax(numerator / denominator, mostIndex);
 		return scale(index).alpha(ratioWithMin(index, 0.2));
