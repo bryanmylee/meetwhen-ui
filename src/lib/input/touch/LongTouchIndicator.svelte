6e2f922830994f362e333efe34b7ced091e0c295
Bryan Lee <bryanmylee@gmail.com>
2022-02-12 01:33:50 +0800

    style: more prominent long touch indicator

--- src/lib/input/touch/LongTouchIndicator.svelte
+++ src/lib/input/touch/LongTouchIndicator.svelte
@@ -6,8 +6,8 @@
 	export let triggerDelay = 100;
 
 	// time to wait after trigger before unmounting element.
-	export const triggerDuration = 200;
-	export const outroDuration = 400;
+	export const triggerDuration = 300;
+	export const outroDuration = 300;
 
 	let expanding = false;
 	let triggered = false;
@@ -35,15 +35,18 @@
 
 <style lang="postcss">
 	div {
-		@apply fixed wh-28 rounded-full pointer-events-none bg-primary-400/50 z-[9999];
+		@apply fixed wh-40 rounded-full bg-primary-400/50;
+		@apply pointer-events-none z-[9999];
+		@apply backdrop-blur-sm;
 		transform: translate(-50%, -50%) scale(0.25);
 		left: var(--x);
 		top: var(--y);
-		transition: transform var(--triggerDelay) ease-out,
+		transition: transform var(--triggerDelay)
+				cubic-bezier(0.94, 0.29, 0.72, 0.91),
 			opacity var(--outroDuration) ease-out var(--triggerDuration);
 
 		&.triggered {
-			@apply border-4 border-primary-400 shadow-lg opacity-0;
+			@apply border-4 border-primary-600 gdark:border-white shadow-lg opacity-0;
 		}
 
 		&.expanding {
