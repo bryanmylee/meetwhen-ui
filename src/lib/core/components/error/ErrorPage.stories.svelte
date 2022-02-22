a32600a11f5f05e6d13ac4f909c4e3bae73763a5
Bryan Lee <bryanmylee@gmail.com>
2022-02-22 20:25:37 +0800

    feat: error page

---
+++ src/lib/core/components/error/ErrorPage.stories.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import ErrorPage from './ErrorPage.svelte';
+</script>
+
+<Meta title="Page/ErrorPage" component={ErrorPage} />
+
+<Template let:args>
+	<ErrorPage status={404} {...args} />
+</Template>
+
+<Story name="Example error page" source />
---
+++ src/routes/__error.svelte
@@ -0,0 +1,38 @@
+<script lang="ts" context="module">
+	export const load: ErrorLoad = ({ error, status }) => {
+		return {
+			props: {
+				error,
+				status,
+			},
+		};
+	};
+</script>
+
+<script lang="ts">
+	import type { ErrorLoad } from '@sveltejs/kit';
+	import { dev } from '$app/env';
+	import type { Maybe } from '$lib/core/types/Maybe';
+	import Head from '$lib/core/components/Head.svelte';
+	import ErrorPage from '$lib/core/components/error/ErrorPage.svelte';
+
+	export let error: Maybe<Error>;
+	export let status: number;
+</script>
+
+<Head subtitle="something went wrong" />
+
+{#if dev}
+	<div>
+		<h1 class="text-title-3">Status</h1>
+		<p>
+			{status}
+		</p>
+		<h1 class="text-title-3">Stack trace</h1>
+		<pre class="font-normal">
+			{error?.stack}
+		</pre>
+	</div>
+{:else}
+	<ErrorPage {status} />
+{/if}
