660053bc163f3895c7e195689c06cab67f165154
Bryan Lee <bryanmylee@gmail.com>
2022-02-23 03:30:35 +0800

    fix: gracefully handle invalid links

--- src/routes/api/link-previews.ts
+++ src/routes/api/link-previews.ts
@@ -1,3 +1,4 @@
+import type { LinkPreviewData } from '$lib/core/types';
 import type { RequestHandler } from '@sveltejs/kit';
 import { getLinkPreview } from 'link-preview-js';
 
@@ -7,7 +8,23 @@ export const post: RequestHandler = async ({ request }) => {
 		if (!Array.isArray(urls) && typeof urls === 'string') {
 			urls = [urls];
 		}
-		const previews = await Promise.all(urls.map((url) => getLinkPreview(url)));
+		const previews: LinkPreviewData[] = await Promise.all(
+			urls.map(async (url) => {
+				try {
+					const preview = (await getLinkPreview(url)) as LinkPreviewData;
+					return {
+						url,
+						favicons: preview.favicons,
+						title: preview.title,
+						description: preview.description,
+					};
+				} catch (error) {
+					return {
+						url,
+					};
+				}
+			}),
+		);
 		return {
 			status: 200,
 			body: JSON.stringify(previews),
