3e6582c7b2999eb783c866e341c067e6ed169e8f
Bryan Lee <bryanmylee@gmail.com>
2022-02-22 21:55:16 +0800

    feat: meeting not found error page

---
+++ src/lib/core/components/error/MeetingNotFound.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button } from '$lib/input';
+
+	export let slug: string;
+</script>
+
+<section class="h-screen-nav flex flex-col justify-center items-center gap-4">
+	<h1 class="slug-title">
+		meetwhen.io/<span class="font-mono text-brand">{slug}</span>
+	</h1>
+	<p class="text-lg mb-4">does not exist</p>
+	<Button href="/">Go back home</Button>
+</section>
+
+<style lang="postcss">
+	.slug-title {
+		@apply text-2xl bg-shade-100 p-4 rounded-xl tracking-wide;
+	}
+</style>
---
+++ src/routes/[slug]/__error.svelte
@@ -0,0 +1,44 @@
+<script lang="ts" context="module">
+	export const load: ErrorLoad = ({ error, status, params }) => {
+		const { slug } = params;
+		return {
+			props: {
+				error,
+				status,
+				slug,
+			},
+		};
+	};
+</script>
+
+<script lang="ts">
+	import type { ErrorLoad } from '@sveltejs/kit';
+	import { dev } from '$app/env';
+	import type { Maybe } from '$lib/core/types/Maybe';
+	import Head from '$lib/core/components/Head.svelte';
+	import ErrorPage from '$lib/core/components/error/ErrorPage.svelte';
+	import MeetingNotFound from '$lib/core/components/error/MeetingNotFound.svelte';
+
+	export let error: Maybe<Error>;
+	export let status: number;
+	export let slug: string;
+</script>
+
+<Head subtitle="meeting not found" />
+
+{#if !dev}
+	<div>
+		<h1 class="text-title-3">Status</h1>
+		<p>
+			{status}
+		</p>
+		<h1 class="text-title-3">Stack trace</h1>
+		<pre class="font-normal">
+			{error?.stack}
+		</pre>
+	</div>
+{:else if status === 404}
+	<MeetingNotFound {slug} />
+{:else}
+	<ErrorPage {status} />
+{/if}
