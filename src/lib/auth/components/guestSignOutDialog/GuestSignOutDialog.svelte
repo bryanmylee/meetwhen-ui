ca620bf66f14595200eed08976ac6cb040f3ace0
Bryan Lee <bryanmylee@gmail.com>
2022-02-25 02:16:22 +0800

    feat: prevent remount

--- src/lib/auth/components/guestSignOutDialog/GuestSignOutDialog.svelte
+++ src/lib/auth/components/guestSignOutDialog/GuestSignOutDialog.svelte
@@ -1,31 +1,28 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import type { User } from 'firebase/auth';
 	import { fetchGuestMeetingPreview } from '$lib/api';
 	import GuestSignOutDialogPure from './GuestSignOutDialogPure.svelte';
+	import type { MeetingPreviewData } from '$lib/api/fetchGuestMeetingPreview';
 
 	export let open = false;
 	export let guestUser: User;
-	$: meetingPreviewPromise = fetchGuestMeetingPreview(guestUser.uid);
+	let meetingPreview: Maybe<MeetingPreviewData> = undefined;
+	onMount(() => {
+		fetchGuestMeetingPreview(guestUser.uid).then(
+			(data) => (meetingPreview = data),
+		);
+	});
 </script>
 
 <div class="flex gap-2 items-center">
-	{#await meetingPreviewPromise}
-		<GuestSignOutDialogPure
-			{open}
-			isLoading
-			guestMeeting={{
-				name: 'loading',
-				slug: 'loading',
-			}}
-			on:sign-out
-		/>
-	{:then meetingPreview}
-		{#if meetingPreview !== undefined}
-			<GuestSignOutDialogPure
-				{open}
-				guestMeeting={meetingPreview}
-				on:sign-out
-			/>
-		{/if}
-	{/await}
+	<GuestSignOutDialogPure
+		{open}
+		isLoading={meetingPreview === undefined}
+		guestMeeting={meetingPreview ?? {
+			name: 'loading',
+			slug: 'loading',
+		}}
+		on:sign-out
+	/>
 </div>
