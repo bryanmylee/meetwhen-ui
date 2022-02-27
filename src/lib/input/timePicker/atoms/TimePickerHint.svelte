e99fd9f07c9a4156472f3a5b85908358a639b025
Bryan Lee <bryanmylee@gmail.com>
2022-02-28 02:24:32 +0800

    fix: disable pointer events on hint

--- src/lib/input/timePicker/atoms/TimePickerHint.svelte
+++ src/lib/input/timePicker/atoms/TimePickerHint.svelte
@@ -54,7 +54,7 @@
 
 <style lang="postcss">
 	.hint {
-		@apply fixed p-2 rounded-lg z-50;
+		@apply fixed p-2 rounded-lg z-50 pointer-events-none;
 		@apply text-white text-label bg-primary-400/50;
 		@apply backdrop-blur-sm;
 	}
